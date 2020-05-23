24ffaf7e7b6715bffc92774700f853bc92e5c3c5
Kenny Lee <kenny_seven@icloud.com>
2020-05-23 13:52:26 +1000

    added mid panel scaffolding

--- src/components/MidPanel.js
+++ src/components/MidPanel.js
@@ -4,15 +4,22 @@ import '../styles/MidPanel.scss';
 function MidPanel() {
   return (
     <div className="midPanelContainer">
-      <div className="midPanelHead">
+      <div className="midPanelHeader">
         <div className="algorithmTitle">Binary Search Tree</div>
         <button type="button" className="quizButton">Quiz</button>
       </div>
       <div className="midPanelBody">
-        Animation Goes here
+        {/* Animation Goes here */}
       </div>
-      <div className="midPanelTail">
-        Buttons and Parameters goes here
+      <div className="midPanelFooter">
+        <div className="controlPanel">
+          Play button, Next button
+        </div>
+        <div className="parameterPanel">
+          ADD: [];
+          DELETE: [];
+          SEARCH: [];
+        </div>
       </div>
     </div>
   );
