bf9f6d3b005281e3a7aa7176a1a7c58ba2114c6e
João Pereira <joaopereira>
2020-08-16 16:04:02 +1000

    Automatic execution functionality implemented.

---
+++ src/components/PlayButton.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { GlobalActions } from '../context/actions';
+import '../styles/NextLineButton.scss';
+
+function PlayButton() {
+  const { dispatch } = useContext(GlobalContext);
+
+  const AutomaticExecution = () => {
+    const interval = setInterval(() => {
+      dispatch(GlobalActions.NEXT_LINE);
+    }, 100);
+    return () => clearInterval(interval);
+  };
+
+  return (
+    <button
+      type="button"
+      className="nextLineButton"
+      onClick={() => AutomaticExecution()}
+    >
+      Play
+    </button>
+  );
+}
+
+export default PlayButton;
