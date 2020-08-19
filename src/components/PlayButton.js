995bda7b9e1ebff312378ee8b55887d78a8e5e94
João Pereira <joaopereira>
2020-08-19 12:22:50 +1000

    Changes to the automatic execution functionality and corresponding testing

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,24 +1,30 @@
 import React, { useContext } from 'react';
+import { fireEvent } from '@testing-library/react';
 import { GlobalContext } from '../context/GlobalState';
 import { GlobalActions } from '../context/actions';
 import '../styles/NextLineButton.scss';
+
+export function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 function PlayButton() {
   const { dispatch, algorithm } = useContext(GlobalContext);
 
   const AutomaticExecution = () => {
-    const interval = setInterval(() => {
-      if (!algorithm.finished) {
-        dispatch(GlobalActions.NEXT_LINE);
-      }
-    }, 100);
-    return () => clearInterval(interval);
+    if (!algorithm.finished) {
+      dispatch(GlobalActions.NEXT_LINE);
+    }
+    sleep(100).then(() => {
+      fireEvent.click(document.getElementById('PlayButton'));
+    });
   };
 
   return (
     <button
       type="button"
       className="nextLineButton"
+      id="PlayButton"
       onClick={() => AutomaticExecution()}
     >
       Play
---
+++ src/components/PlayButton.test.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { GlobalContext } from '../context/GlobalState';
+import { sleep } from './PlayButton';
+import App from '../App';
+
+test('PlayButton execution', () => {
+  const { getByText } = render(<App />);
+  fireEvent.click(getByText(/Play/));
+  sleep(10000).then(() => {
+    const { algorithm } = useContext(GlobalContext);
+    expect(algorithm.finished).toEqual(true);
+  });
+});
