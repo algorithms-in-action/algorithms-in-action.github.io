85836cf34a1a914119d9d9252b3f1650194c9465
João Pereira <joaopereira>
2020-08-19 12:30:35 +1000

    Added comments and minor change

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -4,6 +4,7 @@ import { GlobalContext } from '../context/GlobalState';
 import { GlobalActions } from '../context/actions';
 import '../styles/NextLineButton.scss';
 
+// Function used to force the thread to sleep for n milliseconds.
 export function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
@@ -11,13 +12,18 @@ export function sleep(time) {
 function PlayButton() {
   const { dispatch, algorithm } = useContext(GlobalContext);
 
+  /* After button being clicked, the state of the execution is checked.
+  * If the algorithm is finished, nothing happens.
+  * Otherwise, it simulates the pressing of the button,
+  * making it proceed to the next line.
+  */
   const AutomaticExecution = () => {
     if (!algorithm.finished) {
       dispatch(GlobalActions.NEXT_LINE);
+      sleep(100).then(() => {
+        fireEvent.click(document.getElementById('PlayButton'));
+      });
     }
-    sleep(100).then(() => {
-      fireEvent.click(document.getElementById('PlayButton'));
-    });
   };
 
   return (
