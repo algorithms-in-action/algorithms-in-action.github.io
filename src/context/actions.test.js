b3522a06d6c97deaa831f6683376d61fa5c4850a
Luke Ceddia <lceddia@student.unimelb.edu.au>
2020-08-20 23:53:13 +1000

    Fix actions tests

--- src/context/actions.test.js
+++ src/context/actions.test.js
@@ -7,17 +7,15 @@ describe('LOAD_ALGORITHM action', () => {
     expect(loadedState).toMatchObject({
       id: 'binaryTreeSearch',
       name: 'Binary Tree Search',
-      bookmark: {
-        step: 'start',
-      },
+      bookmark: '1',
     });
   });
 });
 
-// describe('NEXT_LINE action', () => {
-//   it('moves to next line', () => {
-//     const initialState = GlobalActions.LOAD_ALGORITHM(undefined, { name: 'binaryTreeSearch' });
-//     const newState = GlobalActions.NEXT_LINE(initialState);
-//     expect(newState.bookmark).not.toBe(initialState.bookmark);
-//   });
-// });
+describe('NEXT_LINE action', () => {
+  it('moves to next line', () => {
+    const initialState = GlobalActions.LOAD_ALGORITHM(undefined, { name: 'binaryTreeSearch' });
+    const newState = GlobalActions.NEXT_LINE(initialState);
+    expect(newState.bookmark).not.toBe(initialState.bookmark);
+  });
+});
