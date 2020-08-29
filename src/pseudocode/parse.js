fcc4ac7b394e7de5646860cfd0fbebbec2fd23c5
Yinsong Chen <yinsong.chen@student.unimelb.edu.au>
2020-08-29 22:48:17 +1000

    add comment

--- src/pseudocode/parse.js
+++ src/pseudocode/parse.js
@@ -126,7 +126,8 @@ export default function parse(input) {
   const rawCode = removeLineContinuation(input);
   const json = extractCodeBlock(rawCode);
   c = 0;
-  addBookmark(json, 'Main', 0, 0);
+  addBookmark(json, 'Main', 0, 0);\
+  // print pseudocode
   console.log(json);
   return json;
 }
