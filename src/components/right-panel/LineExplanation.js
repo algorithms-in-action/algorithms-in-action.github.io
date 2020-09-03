8a396e363fd975e4d29d92b4e7fa569a2f8cb9da
Yinsong Chen <yinsong.chen@student.unimelb.edu.au>
2020-09-04 02:41:23 +1000

    deal with conflict

--- src/components/right-panel/LineExplanation.js
+++ src/components/right-panel/LineExplanation.js
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react';
+import ReactMarkDown from 'react-markdown/with-html';
 import { GlobalContext } from '../../context/GlobalState';
 import findBookmark from '../../pseudocode/findBookmark';
+import CodeBlock from '../../markdown/code-block';
 
 function LineExplanation() {
   const { algorithm } = useContext(GlobalContext);
   return (
     <div className="lineExplanation">
-      {findBookmark(algorithm.pseudocode, algorithm.bookmark).explanation}
+      <ReactMarkDown
+        source={findBookmark(algorithm.pseudocode, algorithm.bookmark).explanation}
+        escapeHtml={false}
+        renderers={{ code: CodeBlock }}
+      />
     </div>
   );
 }
