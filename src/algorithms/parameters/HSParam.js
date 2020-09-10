4caad510b455c131c8286cc7ed88806d216090f8
Zihan ZHANG <zhazz@student.unimelb.edu.au>
2020-09-10 22:47:08 +1000

    refactor HSParam

--- src/algorithms/parameters/HSParam.js
+++ src/algorithms/parameters/HSParam.js
@@ -1,112 +1,78 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useContext } from 'react';
-import { commaSeparatedNumberListValidCheck, genRandNumList } from './ParamHelper';
 import ControlButton from '../../components/common/ControlButton';
-import ParamMsg from './ParamMsg';
 import '../../styles/Param.scss';
 import { ReactComponent as RefreshIcon } from '../../resources/icons/refresh.svg';
 import { GlobalContext } from '../../context/GlobalState';
 import { GlobalActions } from '../../context/actions';
+import ParamForm from './ParamForm';
+import {
+  commaSeparatedNumberListValidCheck,
+  genRandNumList,
+  successParamMsg,
+  errorParamMsg,
+} from './ParamHelper';
 
-const DEFAULT_ARR = '5,8,10,3,1,6,9,7,2,0,4';
-const HEAP_SORT = 'Heap Sort';
+const DEFAULT_ARR = genRandNumList(10, 1, 100);
+const HEAP_SORT = 'heap Sort';
+const HEAP_SORT_EXAMPLE = 'Example: 0,1,2,3,4';
 
 function HeapsortParam() {
-  const [arrVal, setArrVal] = useState(DEFAULT_ARR);
   const { algorithm, dispatch } = useContext(GlobalContext);
   const disabled = algorithm.hasOwnProperty('visualisers') && algorithm.playing;
+  const [arrVal, setArrVal] = useState(DEFAULT_ARR);
+  const [message, setMessage] = useState(null);
 
-  const [logWarning, setLogWarning] = useState(false);
-  const [logTag, setLogTag] = useState('');
-  const [logMsg, setLogMsg] = useState('');
+  const handleSort = (e) => {
+    e.preventDefault();
+    const inputValue = e.target[0].value;
 
-  const updateParamStatus = (type, val, success) => {
-    if (success) {
-      setLogTag(`${type} success!`);
-      setLogWarning(false);
-      setLogMsg(`Input for ${type} algorithm is valid.`);
+    if (commaSeparatedNumberListValidCheck(inputValue)) {
+      const nodes = inputValue.split`,`.map((x) => +x);
+      setArrVal(nodes);
+      // run heap sort animation
+      dispatch(GlobalActions.RUN_ALGORITHM, { name: 'heapSort', mode: 'sort', nodes });
+      setMessage(successParamMsg(HEAP_SORT));
     } else {
-      setLogTag(`${type} failure!`);
-      setLogWarning(true);
-
-      let warningText = '';
-      warningText += `Input for ${type} algorithm is not valid. `;
-      if (type === HEAP_SORT) {
-        warningText += 'Example: 0,1,2,3,4';
-      } else {
-        warningText += 'Example: 16';
-      }
-
-      setLogMsg(warningText);
-    }
-  };
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-
-    const evtName = evt.target[0].name;
-    const evtVal = evt.target[0].value;
-
-    switch (evtName) {
-      case HEAP_SORT:
-        if (commaSeparatedNumberListValidCheck(evtVal)) {
-          setArrVal(evtVal.split`,`.map((x) => +x));
-          updateParamStatus(HEAP_SORT, arrVal, true);
-          const nodes = typeof arrVal === 'string' ? arrVal.split(',').map((x) => parseInt(x, 10)) : arrVal;
-
-          // run heap sort animation
-          dispatch(GlobalActions.RUN_ALGORITHM, { name: 'heapSort', mode: 'sort', nodes });
-        } else {
-          updateParamStatus(HEAP_SORT, arrVal, false);
-        }
-
-        break;
-      default:
-        break;
+      setMessage(errorParamMsg(HEAP_SORT, HEAP_SORT_EXAMPLE));
     }
   };
 
   return (
     <>
       <div className="form">
-        <form className="formLeft" onSubmit={handleSubmit}>
-          <div className="outerInput">
-            <label className="inputText">
-              <input
-                name={HEAP_SORT}
-                type="text"
-                value={arrVal}
-                data-testid="insertionText"
-                onChange={(e) => setArrVal(e.target.value)}
-              />
-            </label>
-            <div className="btnGrp">
-              <ControlButton
-                icon={<RefreshIcon />}
-                className={disabled ? 'greyRoundBtnDisabled' : 'greyRoundBtn'}
-                id={HEAP_SORT}
-                disabled={disabled}
-                onClick={() => {
-                  const list = genRandNumList(10, 1, 100);
-                  setArrVal(list);
-                }}
-              />
-              <ControlButton
-                className={disabled ? 'blueWordBtnDisabled' : 'blueWordBtn'}
-                type="submit"
-                disabled={disabled}
-              >
-                Sort
-              </ControlButton>
-            </div>
-          </div>
-        </form>
+        {/* Sort input */}
+        <ParamForm
+          formClassName="formLeft"
+          name={HEAP_SORT}
+          value={arrVal}
+          onChange={(e) => setArrVal(e.target.value)}
+          handleSubmit={handleSort}
+        >
+          <ControlButton
+            icon={<RefreshIcon />}
+            className={disabled ? 'greyRoundBtnDisabled' : 'greyRoundBtn'}
+            id={HEAP_SORT}
+            disabled={disabled}
+            onClick={() => {
+              const list = genRandNumList(10, 1, 100);
+              setMessage(null);
+              setArrVal(list);
+            }}
+          />
+          <ControlButton
+            className={disabled ? 'blueWordBtnDisabled' : 'blueWordBtn'}
+            type="submit"
+            disabled={disabled}
+          >
+            Sort
+          </ControlButton>
+        </ParamForm>
       </div>
 
-      {logMsg
-        ? <ParamMsg logWarning={logWarning} logTag={logTag} logMsg={logMsg} />
-        : ''}
+      {/* render success/error message */}
+      {message}
     </>
   );
 }
