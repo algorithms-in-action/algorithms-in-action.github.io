928d4c3a7493e64cb781e1c315fe34fb6b27e5a6
boyu <zby19950926@gmail.com>
2020-10-22 22:05:13 +1100

    change the insturctions content

--- src/components/mid-panel/index.js
+++ src/components/mid-panel/index.js
@@ -26,6 +26,33 @@ function MidPanel({ fontSize, fontSizeIncrement }) {
       <div className="instructionContent">
         <div className="instructionSubContent">1. Enter inputs located in the below middle panel.</div>
         <div className="instructionSubContent">2. Click on the “INSERT” button to see the validation of the input.</div>
+        <div className="instructionSubContent">3. Enter the input needed to be searched and Click on the “SEARCH” button.</div>
+        <diiv className="instructionSubContent">
+          4. For valid input, click on the play button to see the behavior
+          of the algorithm with different parameters.
+        </diiv>
+
+      </div>
+      <div className="instructionSubTitle">
+        How to control the executing speed of an algorithm.
+      </div>
+      <div className="instructionContent">
+        1. Press on the slider bar to change the speed from slow (left) to fast (right).
+      </div>
+    </div>
+  );
+
+  const quickSortInstructions = (
+    <div>
+      <div className="instructionTitle">
+        Instructions
+      </div>
+      <div className="instructionSubTitle">
+        How to use the parameters
+      </div>
+      <div className="instructionContent">
+        <div className="instructionSubContent">1. Enter inputs located in the below middle panel.</div>
+        <div className="instructionSubContent">2. Click on the “SORT” button to see the validation of the input.</div>
         <diiv className="instructionSubContent">
           3. For valid input, click on the play button to see the behavior
           of the algorithm with different parameters.
@@ -41,6 +68,87 @@ function MidPanel({ fontSize, fontSizeIncrement }) {
     </div>
   );
 
+  const heapSortInstructions = (
+    <div>
+      <div className="instructionTitle">
+        Instructions
+      </div>
+      <div className="instructionSubTitle">
+        How to use the parameters
+      </div>
+      <div className="instructionContent">
+        <div className="instructionSubContent">1. Enter inputs located in the below middle panel.</div>
+        <div className="instructionSubContent">2. Click on the “SORT” button to see the validation of the input.</div>
+        <diiv className="instructionSubContent">
+          3. For valid input, click on the play button to see the behavior
+          of the algorithm with different parameters.
+        </diiv>
+
+      </div>
+      <div className="instructionSubTitle">
+        How to control the executing speed of an algorithm.
+      </div>
+      <div className="instructionContent">
+        1. Press on the slider bar to change the speed from slow (left) to fast (right).
+      </div>
+    </div>
+  );
+
+  const transitiveClosureInstructions = (
+    <div>
+      <div className="instructionTitle">
+        Instructions
+      </div>
+      <div className="instructionSubTitle">
+        How to use the parameters
+      </div>
+      <div className="instructionContent">
+        <div className="instructionSubContent">1. Enter SET number located in the below middle panel.</div>
+        <div className="instructionSubContent">2. Click on the “SET” button to change the SET in the right.</div>
+        <div className="instructionSubContent">3. Enter the value in the SET</div>
+        <div className="instructionSubContent">4. Click on the “Run” buton to generate the graph </div>
+        <diiv className="instructionSubContent">
+          5. click on the play button to see the behavior
+          of the algorithm with different parameters.
+        </diiv>
+
+      </div>
+      <div className="instructionSubTitle">
+        How to control the executing speed of an algorithm.
+      </div>
+      <div className="instructionContent">
+        1. Press on the slider bar to change the speed from slow (left) to fast (right).
+      </div>
+    </div>
+  );
+
+  const primInstructions = (
+    <div>
+      <div className="instructionTitle">
+        Instructions
+      </div>
+      <div className="instructionSubTitle">
+        How to use the parameters
+      </div>
+      <div className="instructionContent">
+        <div className="instructionSubContent">1. Enter SET number located in the below middle panel.</div>
+        <div className="instructionSubContent">2. Click on the “SET” button to change the SET in the right.</div>
+        <div className="instructionSubContent">3. Enter the value in the SET</div>
+        <div className="instructionSubContent">4. Click on the “Run” buton to generate the graph </div>
+        <diiv className="instructionSubContent">
+          5. click on the play button to see the behavior
+          of the algorithm with different parameters.
+        </diiv>
+
+      </div>
+      <div className="instructionSubTitle">
+        How to control the executing speed of an algorithm.
+      </div>
+      <div className="instructionContent">
+        1. Press on the slider bar to change the speed from slow (left) to fast (right).
+      </div>
+    </div>
+  );
 
   return (
     <div className="midPanelContainer">
@@ -53,10 +161,10 @@ function MidPanel({ fontSize, fontSizeIncrement }) {
           {(() => {
             switch (algorithm.id) {
               case 'binarySearchTree': return bstInstructions;
-              case 'quickSort': return 'quickSort';
-              case 'heapSort': return 'heapSort';
-              case 'transitiveClosure': return 'transitiveClosure';
-              case 'prim': return 'prim';
+              case 'quickSort': return quickSortInstructions;
+              case 'heapSort': return heapSortInstructions;
+              case 'transitiveClosure': return transitiveClosureInstructions;
+              case 'prim': return primInstructions;
               default: return null;
             }
           }
