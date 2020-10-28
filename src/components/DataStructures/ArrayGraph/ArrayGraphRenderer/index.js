421ffb0bdf32608aeeb140347f8231e5ce6eb609
Zihan ZHANG <zhazz@student.unimelb.edu.au>
2020-10-28 22:55:03 +1100

    chore: remove consoles

--- src/components/DataStructures/ArrayGraph/ArrayGraphRenderer/index.js
+++ src/components/DataStructures/ArrayGraph/ArrayGraphRenderer/index.js
@@ -105,12 +105,6 @@ class GraphRenderer extends Renderer {
       baseHeight * this.zoom,
     ];
 
-    console.log(`View Box: ${viewBox}`);
-
-    console.log(`Center X: ${this.centerX}`);
-    console.log(`Center Y: ${this.centerY}`);
-
-
     return (
       <svg className={switchmode(mode())} viewBox={viewBox} ref={this.elementRef}>
         <defs>
