555cf4b99ed9eed6078f60648ed74f1644bdcdc8
Cameron Wong <camerongmwong@gmail.com>
2021-05-20 23:04:19 +1200

    refactor and fix eslint warnings

--- src/algorithms/parameters/index.js
+++ src/algorithms/parameters/index.js
@@ -4,6 +4,5 @@ export { default as BSTParam } from './BSTParam';
 export { default as QSParam } from './QSParam';
 export { default as PrimsParam } from './PrimsParam';
 export { default as TCParam } from './TCParam';
-export { default as BFSSParam} from './BFSSParam';
+export { default as BFSSParam } from './BFSSParam';
 export { default as QSM3Param } from './QSM3Param';
-
