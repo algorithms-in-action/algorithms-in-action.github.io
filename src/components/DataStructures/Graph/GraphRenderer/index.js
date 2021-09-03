fcbfd6a389660fc52af34cd3cf35e189c24531cb
DanistyWuKids(づ｡◕‿‿◕｡)づ <shabibox@outlook.com>
2021-09-04 02:24:26 +1000

    Strpos update

--- src/components/DataStructures/Graph/GraphRenderer/index.js
+++ src/components/DataStructures/Graph/GraphRenderer/index.js
@@ -145,7 +145,7 @@ class GraphRenderer extends Renderer {
             return (
               <g className={classes(styles.node, selectNode && styles.selected, visitedCount && styles.visited)}
                  key={id} transform={`translate(${x},${y})`}>
-                <rect className={styles.circle} width={2 * nodeRadius} height={2 * nodeRadius} x={-nodeRadius} y={-nodeRadius - 4} />
+                <rect className={styles.circle} width={2 * nodeRadius} height={2 * nodeRadius} x={-nodeRadius} y={-nodeRadius} />
                 <text className={styles.id}>{value}</text>
                 {
                   isWeighted &&
