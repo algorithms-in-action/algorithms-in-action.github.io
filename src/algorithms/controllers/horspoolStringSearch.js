951a592498de4cb2f71b416d57e205c58fe70626
49 <645487820@qq.com>
2021-09-28 12:06:54 +0800

    basic construction of shift table in horspool

--- src/algorithms/controllers/horspoolStringSearch.js
+++ src/algorithms/controllers/horspoolStringSearch.js
@@ -2,21 +2,26 @@
 import { HSSExp } from '../explanations';
 import GraphTracerRect from '../../components/DataStructures/Graph/GraphTracerRect';
 import Array2DTracer from '../../components/DataStructures/Array/Array2DTracer';
+import TwoArray2DTracer from '../../components/DataStructures/Array/TwoArray2DTracer';
 
 export default {
     explanation: HSSExp,
 
     initVisualisers() {
         return {
+            array: {
+                instance: new TwoArray2DTracer('array', null, 'Shift Table'), 
+                order: 0,
+            },
             graph: {
                 instance: new GraphTracerRect('hsp', null, 'Horspool String Search'),
-                order: 0,
+                order: 1,
             },
             /*array: {
                 instance: new Array2DTracer('array', null, 'Matrix'),
                 order: 1,
             },*/
-            
+
         };
     },
 
@@ -30,6 +35,16 @@ export default {
         // searchString and findString are stored in the same array
         // to get element in findString, add the length of searchString to the index
         chunker.add('1', (vis, n) => {
+            const shiftTable=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','space'];
+            vis.array.set([shiftTable.slice(0,13),new Array(13).fill(findString.length)],
+                          [shiftTable.slice(13,27),new Array(14).fill(findString.length)],
+                          'horspools');
+            //temp,this block should be in chunker 2 with dispatch
+            for (let i=0;i<findString.length-1;i++)
+            {
+              vis.array.patch(shiftTable.indexOf(findString[i].toUpperCase()),1,findString.length-(i+1));
+            }
+
             vis.graph.addNode(stringCount, searchString[0], 'box');
             stringCount++;
             for (let i = 1; i < searchString.length; i++) {
---
+++ src/components/DataStructures/Array/TwoArray2DTracer.js
@@ -0,0 +1,119 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-mixed-operators */
+/* eslint-disable arrow-parens */
+/* eslint-disable operator-linebreak */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-max-props-per-line */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-first-prop-new-line */
+/* eslint-disable max-len */
+/* eslint-disable object-curly-newline */
+
+import Tracer from '../common/Tracer';
+import TwoArray2DRenderer from './TwoArray2DRenderer';
+
+class Element {
+  constructor(value) {
+    this.value = value;
+    this.patched = false;
+    this.selected = false;
+    this.sorted = false;
+  }
+}
+
+class TwoArray2DTracer extends Tracer {
+  getRendererClass() {
+    return TwoArray2DRenderer;
+  }
+
+  /**
+   * @param {array} array2d
+   * @param {string} algo used to mark if it is a specific algorithm
+   */
+  set(array2d = [],array2d1=[], algo) {
+    this.data = array2d.map(array1d => [...array1d].map(value => new Element(value)));
+    this.data1 = array2d1.map(array1d => [...array1d].map(value => new Element(value)));
+    this.algo = algo;
+    super.set();
+  }
+
+  getposition(x,y){
+    let longestRow = this.data.reduce((longestRow, row) => longestRow.length < row.length ? row : longestRow, []);
+    let len = longestRow.length;
+    var xx=x;
+    var pos=0;
+    if (x>=len){xx=x-len;pos=1;}
+    return [pos,xx,y];
+  }
+
+  patch(x, y, v = this.data[x][y].value) {
+    let rr=this.getposition(x,y);
+    var pos=rr[0];
+    var xx = rr[2];//x is actually the vertical one.... need to refactor here.
+    var yy = rr[1];
+    if (pos===0)
+    {
+      if (!this.data[xx][yy]) {this.data[xx][yy] = new Element();}
+      this.data[xx][yy].value = v;
+      this.data[xx][yy].patched = true;
+    }
+    else
+    {
+      if (!this.data1[xx][yy]) {this.data1[xx][yy] = new Element();}
+      this.data1[xx][yy].value = v;
+      this.data1[xx][yy].patched = true;
+    }
+
+  }
+
+  depatch(x, y) {
+    x=this.getposition(x,y);
+    this.data[x][y].patched = false;
+  }
+
+  // used to highlight sorted elements
+  sorted(x, y) {
+    if (!this.data[x][y]) this.data[x][y] = new Element();
+    this.data[x][y].sorted = true;
+  }
+
+  select(sx, sy, ex = sx, ey = sy) {
+    for (let x = sx; x <= ex; x++) {
+      for (let y = sy; y <= ey; y++) {
+        this.data[x][y].selected = true;
+      }
+    }
+  }
+
+  selectRow(x, sy, ey) {
+    this.select(x, sy, x, ey);
+  }
+
+  selectCol(y, sx, ex) {
+    this.select(sx, y, ex, y);
+  }
+
+  deselect(sx, sy, ex = sx, ey = sy) {
+    for (let x = sx; x <= ex; x++) {
+      for (let y = sy; y <= ey; y++) {
+        this.data[x][y].selected = false;
+      }
+    }
+  }
+
+  deselectRow(x, sy, ey) {
+    this.deselect(x, sy, x, ey);
+  }
+
+  deselectCol(y, sx, ex) {
+    this.deselect(sx, y, ex, y);
+  }
+}
+
+export default TwoArray2DTracer;
