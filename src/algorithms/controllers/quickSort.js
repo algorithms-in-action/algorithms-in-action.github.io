dfea608c71532add0882e7c146f95596f9c0e548
LawrenceLin2333 <562775064@qq.com>
2021-10-03 19:22:50 +0800

    code style

--- src/algorithms/controllers/quickSort.js
+++ src/algorithms/controllers/quickSort.js
@@ -85,7 +85,7 @@ export default {
       swapAction(13, i, right);
       chunker.add(13, (vis, i) => {
         vis.array.sorted(i);
-      }, [i])
+      }, [i]);
       return [i, a]; // Return [pivot location, array values]
     }
 
@@ -102,11 +102,11 @@ export default {
         chunker.add(4);
         QuickSort(a, p + 1, right, `${right}/${p + 1}`);
       }
-      //array of size 1, already sorted
-      else if (left < array.length){
+      // array of size 1, already sorted
+      else if (left < array.length) {
         chunker.add(2, (vis, left) => {
           vis.array.sorted(left);
-        }, [left])
+        }, [left]);
       }
       return a; // Facilitates testing
     }
--- src/algorithms/controllers/quickSortM3.js
+++ src/algorithms/controllers/quickSortM3.js
@@ -94,7 +94,7 @@ export default {
       swapAction(13, i, right - 1);
       chunker.add(13, (vis, i) => {
         vis.array.sorted(i);
-      }, [i])
+      }, [i]);
       return [i, a]; // Return [pivot index, array values]
     }
 
@@ -113,11 +113,11 @@ export default {
         chunker.add(4);
         QuickSort(a, p + 1, right, `${right}/${p + 1}`);
       }
-      //array of size 1, already sorted
-      else if (left < array.length){
+      // array of size 1, already sorted
+      else if (left < array.length) {
         chunker.add(2, (vis, left) => {
           vis.array.sorted(left);
-        }, [left])
+        }, [left]);
       }
       return a; // Facilitates testing
     }
