ddc6af8b50c8f6d406a04ca0dfc0c94b1c36b2a9
DanistyWuKids(づ｡◕‿‿◕｡)づ <shabibox@outlook.com>
2021-10-18 16:10:53 +1100

    Adding about page info

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,8 @@
 import React from 'react';
 import Header from './Header';
 import Team from './Team';
-import { team } from '../../resources/About';
+import Team2021s2 from './Team2021s2';
+import { team, team2021s2 } from '../../resources/About';
 import '../../styles/About.scss';
 import { ReactComponent as Wave } from '../../assets/icons/wave.svg';
 import ClientSection from './ClientSection';
@@ -22,11 +23,9 @@ const About = () => {
         </div>
         <ClientSection />
         <AppSection />
-        <div className="aboutContainer">
-          <Team teammembers={team} />
-        </div>
+        <div className="aboutContainer"><Team teammembers={team} /></div>
+        <div className="aboutContainer"><Team2021s2 teammembers={team2021s2} /></div>
         <Contribute />
-
       </div>
     </>
   );
