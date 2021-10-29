a2a9143e1bd36fd80af1cb187c5ea1ff1878392b
ZIYIZHANG2 <88768285+ZIYIZHANG2@users.noreply.github.com>
2021-10-29 08:58:54 +0000

    Deploying to master from  @ 533cdafdb194494ae0bcba2ccd18a2ef321ad344 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d04c76d4d8afcf8e26fbf48f86bf4e5.js"
+  "/precache-manifest.f8cd58d770c8b3b9dc91ca942a9a5ae6.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/media/BFSSInfo.e66d7f66.md
+++
@@ -1,3 +0,0 @@
-# Extra Info
-
-<a href="https://www.geeksforgeeks.org/naive-algorithm-for-pattern-searching/" target="_blank">Geeks for Geeks/ Brute Force String Search</a>
