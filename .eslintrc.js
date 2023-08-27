62d58ac9b517aba9c4c31ea6628031de4f2eb0b2
rodenwild <rodenwild@gmail.com>
2023-08-27 12:54:50 +1000

    Reformatting + Formatting config change

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,36 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "airbnb/hooks"],
+  extends: ['plugin:react/recommended', 'airbnb', 'airbnb/hooks'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"],
-    "no-continue": 0,
-    "no-case-declarations": 0,
-    "no-underscore-dangle": 0,
-    "linebreak-style": 0,
-    "comma-dangle": "off"
-  }
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-continue': 0,
+    'no-case-declarations': 0,
+    'no-underscore-dangle': 0,
+    'linebreak-style': 0,
+    'comma-dangle': 'off',
+    'no-trailing-spaces': 'off',
+    'no-multi-spaces': 'off',
+    'implicit-arrow-linebreak': 'off',
+  },
 };
