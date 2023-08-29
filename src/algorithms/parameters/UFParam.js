ba6d5c3002cec2bde2f6c483aa214823e472f03a
calberti <calberti@student.unimelb.edu.au>
2023-08-29 11:59:58 +1000

    feat: create parameter ui

--- src/algorithms/parameters/UFParam.js
+++ src/algorithms/parameters/UFParam.js
@@ -1,61 +1,133 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { GlobalContext } from '../../context/GlobalState';
-import { GlobalActions } from '../../context/actions';
-import '../../styles/Param.scss';
-import {
-  singleNumberValidCheck,
-  genRandNumList,
-  successParamMsg,
-  errorParamMsg,
-} from './helpers/ParamHelper';
+import React, { useState, useEffect, useContext} from 'react';
 
-const useStyles = makeStyles((theme) => ({
+
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+import SingleValueParam from './helpers/SingleValueParam';
+import DualValueParam from './helpers/DualValueParam';
+
+import { withStyles } from '@material-ui/core/styles';
+import Radio from '@material-ui/core/Radio';
+
+
+import '../../styles/Param.scss';
+
+const SET_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const DEFAULT_UNION = "5-7,3-4,9-8,3-8"
+const DEFAULT_FIND = "2"
+
+const UNION_FIND = 'Union Find';
+const FIND = 'Find'
+const UNION = 'Union'
+const FIND_EXAMPLE = 'Please follow the example provided: 0,1';
+const UNION_EXAMPLE = 'Please follow the example provided: 0,1';
+
+
+const DEFAULT_SIZE = 4;
+// path compression:
+const UNCHECKED = {on: false, off: false};
+
+const BlueRadio = withStyles({
   root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      // width: '25ch',
+    color: '#2289ff',
+    '&$checked': {
+      color: '#027aff',
     },
   },
-}));
+  checked: {},
+  // eslint-disable-next-line react/jsx-props-no-spreading
+})((props) => <Radio {...props} />);
 
-function SingleValueParam() {
-  const classes = useStyles();
+function UFParam() {
   const [message, setMessage] = useState(null);
-  const [value, setValue] = useState(0);
+  const [nodes, setNodes] = useState(SET_VALUES);
+  const [string, setString] = useState(DEFAULT_UNION);
+  // set up array here :)
+  // const [nodes, SetNodes] = useState(SET_VALUES;
+
+  const [pathCompression, setPathCompression] = useState( {
+    on: false,
+    off: true,
+  });
 
   const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  const handleParamSubmit = (e) => {
-    e.preventDefault();
-    if (singleNumberValidCheck(value)) {
-      setMessage(successParamMsg);
-    } else {
-      setMessage(errorParamMsg);
+    switch (e.target.name) {
+      case 'on':
+        // path compression on
+        break;
+      case 'off':
+        // remove path compression from pseudocode
+        break;
+      default:
     }
-  };
+
+    setPathCompression({ ...UNCHECKED, [e.target.name]: true });
+  }
+
+  useEffect(
+    () => {
+      document.getElementById('startBtnGrp').click();
+    },
+    [pathCompression],
+  );
 
   return (
-    <GlobalContext.Consumer>
-      {(context) => (
-        <form className={classes.root} noValidate autoComplete="off" onSubmit={handleParamSubmit}>
-          <input
-            type="number"
-            name="value"
-            value={value}
-            onChange={handleChange}
-            className="param-input"
-            placeholder="Enter a value"
+    <>
+
+      <div className="form">
+      
+        <DualValueParam
+            name="unionFind"
+            buttonName="Union"
+            mode="union"
+            formClassName="formLeft"
+            DEFAULT_VAL={DEFAULT_UNION}
+            ALGORITHM_NAME={UNION}
+            EXAMPLE={UNION_EXAMPLE}
+            setMessage={setMessage}
           />
-          <input type="submit" value="Submit" className="param-submit" />
-          {message}
-        </form>
-      )}
-    </GlobalContext.Consumer>
+
+      {/* might need to add some CSS or shit to centre the find. */}
+      <SingleValueParam
+          name="unionFind"
+          buttonName="Find"
+          mode="search"
+          formClassName="formRight" // i see (this is the form name within div)
+          DEFAULT_VAL={DEFAULT_FIND}
+          ALGORITHM_NAME={FIND}
+          EXAMPLE={FIND_EXAMPLE}
+          setMessage={setMessage}
+        />
+      </div>
+
+      <span className="generalText">Path compression: &nbsp;&nbsp;</span>
+      <FormControlLabel
+        control={(
+          <BlueRadio
+            checked={pathCompression.on}
+            onChange={handleChange}
+            name="on"
+          />
+        )}
+        label="On"
+        className="checkbox"
+      />
+      <FormControlLabel
+        control={(
+          <BlueRadio
+            checked={pathCompression.off}
+            onChange={handleChange}
+            name="off"
+          />
+        )}
+        label="Off"
+        className="checkbox"
+      /> 
+      {/* render success/error message */}
+      {message}
+    </>
   );
 }
 
-export default SingleValueParam;
+export default UFParam;
---
+++ src/algorithms/parameters/helpers/DualValueForm.js
@@ -0,0 +1,68 @@
+//SIM Event - DanistyWuKids
+import React, { useContext,useEffect } from 'react';
+import ControlButton from '../../../components/common/ControlButton';
+import { closeInstructions } from '../../../components/mid-panel/helper';
+import { GlobalContext } from '../../../context/GlobalState';
+import '../../../styles/Param.scss';
+
+
+const mouseClickEvents = ['mousedown', 'click', 'mouseup'];
+function simulateMouseClick(element){
+  mouseClickEvents.forEach(mouseEventType => element.dispatchEvent(new MouseEvent(mouseEventType, {view: window,bubbles: true,cancelable: true,buttons: 1})));
+}
+
+function DualValueForm(props) {
+    const {
+        formClassName, name, buttonName, input1, input2, textInput, onAdd, handleSubmit, disabled
+    } = props;
+
+    const { algorithm } = useContext(GlobalContext);
+
+    const closeInstructionsFun = () => {
+        if (algorithm.name === 'Quicksort') {
+          // setQuicksortPlay(false)
+          sessionStorage.setItem('isPivot', false);
+          sessionStorage.setItem('quicksortPlay', false);
+        }
+        closeInstructions();
+      };
+
+    useEffect(() => {var element = document.querySelector('button[id="startBtnGrp"]');simulateMouseClick(element);},[]);
+
+    return (
+        <form className={formClassName} onSubmit={handleSubmit}>
+            <div className="outerInput">
+                <div className="union">
+                    <div className="unionAddInput">
+                        <div className="inputGrouped">
+                        <input type="text" value={input1.value} onChange={input1.onChange} 
+                        placeholder='Set 1'/>
+                        { /*name={name}... we may need to add 'name', not certain.*/}
+                        <input type="text" value={input2.value} onChange={input2.onChange}
+                        placeholder='Set 2'/>
+                        </div>
+                        <button type="button" onClick={onAdd} className="btn greyWordBtn">Add</button>
+                    </div>
+
+                    <div className="unionTextInput">
+                        <input type="text" value={textInput.value} onChange={textInput.onChange} />
+                        <div className="btnGrp">
+                            <ControlButton
+                            className={disabled ? 'blueWordBtnDisabled' : 'blueWordBtn'}
+                            onClick={closeInstructionsFun}
+                            id="startBtnGrp"
+                            type="submit"
+                            disabled={disabled}
+                            >
+                            {buttonName}
+                            </ControlButton>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
+
+
+}
+export default DualValueForm;
