e848b488b37ca4ac3c596c5e9a165f4118b67623
David Sha <david.sha2002@gmail.com>
2023-09-10 16:07:49 +1000

    chore: adhere to conventions

--- src/algorithms/controllers/unionFindUnion.js
+++ src/algorithms/controllers/unionFindUnion.js
@@ -14,70 +14,67 @@ export default {
         order: 0,
       },
       // insert tree here
-
     };
   },
 
+  find(chunker, parentArr, n, bookmark) {
+    // there is a bug here
+    chunker.add(bookmark, (vis) => {
+      vis.array.select(1, n - 1);
+    });
 
-  
-  run(chunker, params) {
-    const unionOperations = params.target;
-    
-    // initialise parent array
-    let parentArray = [...N_ARRAY]
-    
-    // setting up the arrays
-    chunker.add('1', (vis, array) => {
-      vis.array.set(array);
-    }, [[N_ARRAY, parentArray]]); // will add a third array for rank here
-
-    // applying union operations
-    for (let i = 0; i < unionOperations.length; i++) {
-      union(chunker, parentArray, unionOperations[i][0], unionOperations[i][1]);
+    while (parentArr[n - 1] !== n) {
+      // eslint-disable-next-line no-param-reassign
+      n = parentArr[n - 1];
     }
 
-
+    return n;
   },
 
+  union(chunker, parentArray, x, y) {
+    const root1 = this.find(chunker, parentArray, x, '2');
+    const root2 = this.find(chunker, parentArray, y, '3');
+
+    chunker.add('4', (vis) => {
+      vis.array.select(1, root1 - 1);
+      vis.array.select(1, root2 - 1); // is there a way to avoid -1 here?
+    });
+
+    if (root1 !== root2) {
+      // eslint-disable-next-line no-param-reassign
+      parentArray[root2 - 1] = root1;
+
+      // update array
+      chunker.add(
+        '5',
+        (vis, array) => {
+          vis.array.set(array);
+        },
+        [[N_ARRAY, parentArray]],
+      );
+    }
+  },
+
+  run(chunker, params) {
+    const unionOperations = params.target;
+
+    // initialise parent array
+    const parentArray = [...N_ARRAY];
+
+    // setting up the arrays
+    chunker.add(
+      '1',
+      (vis, array) => {
+        vis.array.set(array);
+      },
+      [[N_ARRAY, parentArray]],
+    ); // will add a third array for rank here
+
+    // applying union operations
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < unionOperations.length; i++) {
+      // eslint-disable-next-line no-undef
+      this.union(chunker, parentArray, unionOperations[i][0], unionOperations[i][1]);
+    }
+  },
 };
-
-function find(chunker, parentArr, n) {
-
-  // there is a bug here
-  chunker.add('1', (vis) => {
-    vis.array.select(1, n-1);
-  },);
-
-  while (parentArr[n-1] !== n) {
-
-      n = parentArr[n-1];
-  }
-
-  return n;
-}
-
-function union(chunker, parentArray, x, y) {
-
-
-  let root1 = find(chunker, parentArray, x);
-  let root2 = find(chunker, parentArray, y);
-
-
-  chunker.add('1', (vis) => {
-      vis.array.select(1, root1-1);
-      vis.array.select(1, root2-1); // is there a way to avoid -1 here?
-  },);
-
-  if (root1 !== root2) {
-
-    parentArray[root2-1] = root1;
-
-    // update array
-    chunker.add('1', (vis, array) => {
-      vis.array.set(array);
-    }, [[N_ARRAY, parentArray]]);
-  }
-
-}
-
-
