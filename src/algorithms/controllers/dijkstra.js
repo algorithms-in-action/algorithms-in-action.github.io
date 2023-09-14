fa22558775e71856c62fa417a0875999b8d40a7a
thomasjtHe <jintingh@student.unimelb.edu.au>
2023-09-14 15:20:23 +1000

    dijkstra code moved to astar, heuristic array yet to be implemented

--- src/algorithms/controllers/dijkstra.js
+++ src/algorithms/controllers/dijkstra.js
@@ -1,15 +1,6 @@
-// TODO
-
-/* eslint-disable brace-style */
-/* eslint-disable no-plusplus */
-/* eslint-disable comma-dangle */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable no-multi-spaces,indent,prefer-destructuring */
 import GraphTracer from '../../components/DataStructures/Graph/GraphTracer';
 import Array2DTracer from '../../components/DataStructures/Array/Array2DTracer';
 import { chunk } from 'lodash';
-
-// merge test 
 
 export default {
   initVisualisers() {
@@ -24,75 +15,6 @@ export default {
       },
     };
   },
-
-  /**
-   *
-   * @param {object} chunker
-   * @param {array} nodes array of numbers needs to be sorted
-   */
-  run1(chunker, { matrix }) {
-    const E = [...matrix];
-    const vertex = matrix.length;
-
-    let weight = new Array(matrix.length);
-    for (let i = 0; i < weight.length; i += 1) {
-      weight[i] = new Array(matrix.length);
-    }
-
-    const cost = new Array(matrix.length);
-    const pending = new Array(matrix.length);
-    const prev = new Array(matrix.length);
-    const pq = new Array(matrix.length);
-    const nodes = [];
-    let n;
-    let miniIndex;
-    const minCosts = [];
-    const parents = []; 
-
-    chunker.add(
-      1,
-      (vis, array) => {
-        vis.graph.directed(false);
-        vis.graph.weighted(true);
-        vis.graph.set(array, Array.from({ length: matrix.length }, (v, k) => (k + 1)));
-      },
-      [E]
-    );
-  
-
-    let i;
-    weight = [...E];
-    n = vertex;
-    for (i = 0; i < n; i += 1) {
-      cost[i] = Infinity;
-      prev[i] = 0;
-      pending[i] = 1;
-    }
-    
-    minCosts.push('Cost[i]');  // initialize the cost list
-    nodes.push('i'); // initialize the first row of list
-    parents.push('Parent[i]'); // initialize the parents list
-    for (i = 0; i < n; i += 1) {
-      pq[i] = i;
-      nodes[i + 1] = i + 1;
-      minCosts.push(Infinity);
-      parents.push(0);
-    }
-    
-    miniIndex = 1; // point the mini index in the pq cost
-    /* the chunker add select the minimum cost one */
-    chunker.add(
-        2,
-        (vis, v, w) => {
-          vis.array.set(v, 'dijkstra');
-        },
-        [[nodes, parents, minCosts], miniIndex]
-    );
-    
-    
-
-
-  }, 
 
   run(chunker, { matrix }) {
     const numVertices = matrix.length;
