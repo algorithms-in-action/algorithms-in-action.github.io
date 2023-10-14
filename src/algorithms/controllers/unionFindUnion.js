f8871656503958d1ba82f7faf9f2888045eddc11
David Sha <david.sha2002@gmail.com>
2023-10-14 15:48:46 +1100

    chore: follow format convention

--- src/algorithms/controllers/unionFindUnion.js
+++ src/algorithms/controllers/unionFindUnion.js
@@ -102,9 +102,9 @@ export default {
   /**
    * Highlight the current node.
    * @param {*} visObj
-   * @param {*} index1 
-   * @param {*} index2 
-   * @param {*} colour 
+   * @param {*} index1
+   * @param {*} index2
+   * @param {*} colour
    */
   highlight(visObj, index1, index2, colour) {
     if (visObj.key === 'array') {
