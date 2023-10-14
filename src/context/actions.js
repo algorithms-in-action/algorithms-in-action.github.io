d63673dc01b0a95d7f157d72a32d82937a041f7b
calberti <calberti@student.unimelb.edu.au>
2023-10-14 15:15:12 +1100

    fix: proposed change to explanation behaviour

--- src/context/actions.js
+++ src/context/actions.js
@@ -165,7 +165,8 @@ export const GlobalActions = {
     const chunker = new Chunker(() => controller[params.mode].initVisualisers(params));
     controller[params.mode].run(chunker, params);
     const bookmarkInfo = chunker.next();
-    const firstLineExplan = findBookmark(procedurePseudocode, bookmarkInfo.bookmark).explanation;
+    //const firstLineExplan = findBookmark(procedurePseudocode, bookmarkInfo.bookmark).explanation;
+    const firstLineExplan = null;
     previousState = [];
 
     return {
@@ -191,19 +192,19 @@ export const GlobalActions = {
     let result;
 
     let triggerPauseInCollapse = false;
-    if(typeof playing === 'object'){
+    if (typeof playing === 'object') {
       triggerPauseInCollapse = playing.triggerPauseInCollapse;
       playing = playing.playing;
     }
 
     do {
       result = state.chunker.next(triggerPauseInCollapse);
-      if(!triggerPauseInCollapse){
+      if (!triggerPauseInCollapse) {
         result.pauseInCollapse = false;
       }
     } while (
       !result.pauseInCollapse &&
-      !result.finished && 
+      !result.finished &&
       !isBookmarkVisible(state.pseudocode, state.collapse[state.id.name][state.id.mode], result.bookmark)
     );
     if (result.finished) {
