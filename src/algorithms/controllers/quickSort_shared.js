abdb2312427ee81ba30183874add3b3cfd2254aa
Henry Routson <hroutson@student.unimelb.edu.au>
2023-10-21 15:18:01 +1100

    BUGGED seperate code for median of 3

--- src/algorithms/controllers/quickSort_shared.js
+++ src/algorithms/controllers/quickSort_shared.js
@@ -27,30 +27,50 @@ const STACK_FRAME_COLOR = {
 // bookmarks (id) into the REAL file for quicksort
 // (search \\B and find quicksort)
 // keep up to date with this file, ideally this would auto generate
-// tagged with M3 if only in median of 3
+/* 
+  MEDIAN3_
+  SHARED_ if same instruction between MEDIAN3 and RIGHT_PIVOT
+  RIGHT_P_
+*/
+
 const QS_BOOKMARKS = {
-  quicksort_left_to_right: 1,
-  if_left_less_right: 2,
-  quicksort_left_to_i_minus_1: 3,
-  quicksort_i_plus_1_to_right: 4,
-  set_pivot_to_value_at_array_indx_right: 5,
-  while_i_less_j: 6,
-  incri_i_until_array_index_i_greater_eq_pivot: 7,
-  decri_j_until_array_index_j_less_i: 8,
-  if_j_greater_i: 9,
-  swap_array_i_j_vals: 10,
-  set_i_left_minus_1: 11,
-  set_j_right: 12,
-  swap_pivot_into_correct_position: 13,
-  M3_mid_to_middle_index: 14, // M3
-  M3_first_swap_A_idx_left_with_A_idx_mid: 15, // 
-  M3_swap_A_idx_right_with_A_idx_mid: 16,
-  M3_second_swap_A_idx_left_with_A_idx_mid: 17,
-  M3_swap_A_idx_mid_with_A_idx_right_minus_1: 18,
-  done_qs: 19,
-  M3_first_if_A_idx_left_greater_A_idx_right: 20,
-  M3_if_A_idx_mid_greater_A_idx_right: 21,
-  M3_second_if_A_idx_left_greater_A_idx_right: 22,
+  SHARED_quicksort_left_to_right: 1,
+  SHARED_if_left_less_right: 2,
+  SHARED_quicksort_left_to_i_minus_1: 3,
+  SHARED_quicksort_i_plus_1_to_right: 4,
+
+  // TODO
+  RIGHT_P_set_pivot_to_value_at_array_indx_right: 5,
+  MEDIAN3_set_pivot_to_value_at_array_indx_right_minus_1: 5, 
+
+  SHARED_while_i_less_j: 6,
+  SHARED_incri_i_until_array_index_i_greater_eq_pivot: 7,
+  SHARED_decri_j_until: 8, // shortened name
+  SHARED_if_j_greater_i: 9,
+  SHARED_swap_array_i_j_vals: 10,
+
+  // TODO
+  RIGHT_P_set_i_left_minus_1: 11,
+  MEDIAN3_set_i_left: 11,
+
+  // TODO
+  RIGHT_P_set_j_right: 12,
+  MEDIAN3__set_j_right_minus_1: 12,
+
+
+  // TODO
+  RIGHT_P_swap_pivot_into_position_ie_index_i_and_right: 13,
+  MEDIAN3_swap_pivot_into_position_ie_index_i_and_right_minus_1: 13,
+
+  MEDIAN3_mid_to_middle_index: 14, 
+  MEDIAN3_first_swap_A_idx_left_with_A_idx_mid: 15, 
+  MEDIAN3_swap_A_idx_right_with_A_idx_mid: 16,
+  MEDIAN3_second_swap_A_idx_left_with_A_idx_mid: 17,
+  MEDIAN3_swap_A_idx_mid_with_A_idx_right_minus_1: 18,
+  SHARED_done_qs: 19,
+  MEDIAN3_first_if_A_idx_left_greater_A_idx_right: 20,
+  MEDIAN3_if_A_idx_mid_greater_A_idx_right: 21,
+  MEDIAN3_second_if_A_idx_left_greater_A_idx_right: 22,
 };
 
 // ----------------------------------------------------------------------------------------------------------------------------
@@ -297,7 +317,7 @@ export function run_QS(is_qs_median_of_3) {
 
 
           // assigning the pivot as the midpoint calculated above
-          chunker.add(QS_BOOKMARKS.M3_mid_to_middle_index, (vis, cur_mid, cur_left, cur_right) => {
+          chunker.add(QS_BOOKMARKS.MEDIAN3_mid_to_middle_index, (vis, cur_mid, cur_left, cur_right) => {
             highlight(vis, cur_mid, false);
             highlight(vis, cur_left, false);
             highlight(vis, cur_right, false);
@@ -305,30 +325,32 @@ export function run_QS(is_qs_median_of_3) {
           [mid, left, right],); 
 
           // if a[left] > a[mid]
-          chunker.add(QS_BOOKMARKS.M3_first_if_A_idx_left_greater_A_idx_right); 
+          chunker.add(QS_BOOKMARKS.MEDIAN3_first_if_A_idx_left_greater_A_idx_right); 
           if (a[left] > a[mid]) {
-            swapAction(QS_BOOKMARKS.M3_first_swap_A_idx_left_with_A_idx_mid, left, mid);
+            swapAction(QS_BOOKMARKS.MEDIAN3_first_swap_A_idx_left_with_A_idx_mid, left, mid);
           }
 
           // if A[mid] > A[right]
-          chunker.add(QS_BOOKMARKS.M3_if_A_idx_mid_greater_A_idx_right);
+          chunker.add(QS_BOOKMARKS.MEDIAN3_if_A_idx_mid_greater_A_idx_right);
           if (a[mid] > a[right]) {
-            swapAction(QS_BOOKMARKS.M3_swap_A_idx_right_with_A_idx_mid, right, mid);
+            swapAction(QS_BOOKMARKS.MEDIAN3_swap_A_idx_right_with_A_idx_mid, right, mid);
 
             //if A[left] > A[mid]
-            chunker.add(QS_BOOKMARKS.M3_second_if_A_idx_left_greater_A_idx_right);
+            chunker.add(QS_BOOKMARKS.MEDIAN3_second_if_A_idx_left_greater_A_idx_right);
             if (a[left] > a[mid]) {
-              swapAction(QS_BOOKMARKS.M3_second_swap_A_idx_left_with_A_idx_mid, left, mid);
+              swapAction(QS_BOOKMARKS.MEDIAN3_second_swap_A_idx_left_with_A_idx_mid, left, mid);
             }
           }
 
           // Swap(A[mid], A[right - 1])
-          swapAction(QS_BOOKMARKS.M3_swap_A_idx_mid_with_A_idx_right_minus_1, mid, right-1);
+          swapAction(QS_BOOKMARKS.MEDIAN3_swap_A_idx_mid_with_A_idx_right_minus_1, mid, right-1);
 
           // pivot <- A[right - 1]
 
           pivot_index = right-1
-          chunker.add(QS_BOOKMARKS.set_pivot_to_value_at_array_indx_right, (vis, cur_right, cur_left) => {
+
+          // TODO
+          chunker.add(QS_BOOKMARKS.MEDIAN3_set_pivot_to_value_at_array_indx_right_minus_1, (vis, cur_right, cur_left) => {
             unhighlight(vis, cur_right, false);
             unhighlight(vis, cur_right -1, false);
             unhighlight(vis, cur_left, false);
@@ -338,8 +360,11 @@ export function run_QS(is_qs_median_of_3) {
           // TODO why is this right -1 ?
 
         } else {
+
+          pivot_index = right
+
           chunker.add(
-            QS_BOOKMARKS.set_pivot_to_value_at_array_indx_right,
+            QS_BOOKMARKS.RIGHT_P_set_pivot_to_value_at_array_indx_right,
             (vis, cur_right, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot, cur_depth) => {
 
               assert(cur_right === cur_pivot);
@@ -352,8 +377,10 @@ export function run_QS(is_qs_median_of_3) {
 
         // pick pivot end --------
 
+
+        // TODO
         chunker.add(
-          QS_BOOKMARKS.set_i_left_minus_1,
+          QS_BOOKMARKS.set_i_left_minus_1, 
           (vis, i1, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot, cur_depth) => {
 
             if (i1 >= 0) {
@@ -367,6 +394,7 @@ export function run_QS(is_qs_median_of_3) {
           [i, real_stack, finished_stack_frames, i, undefined, right, depth],
         );
 
+        // TODO
         chunker.add(
           QS_BOOKMARKS.set_j_right,
           (vis, j1, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot, cur_depth) => {
@@ -383,14 +411,14 @@ export function run_QS(is_qs_median_of_3) {
 
       while (i < j) {
         if (boolShouldAnimate()) {
-          chunker.add(QS_BOOKMARKS.while_i_less_j);
+          chunker.add(QS_BOOKMARKS.SHARED_while_i_less_j);
         }
         do {
           i += 1;
 
           if (boolShouldAnimate()) {
             chunker.add(
-              QS_BOOKMARKS.incri_i_until_array_index_i_greater_eq_pivot,
+              QS_BOOKMARKS.SHARED_incri_i_until_array_index_i_greater_eq_pivot,
               (vis, i1, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot, cur_depth) => {
 
                 // assign_i_j already takes into account the isPartitionExpanded
@@ -406,7 +434,7 @@ export function run_QS(is_qs_median_of_3) {
           j -= 1;
           if (boolShouldAnimate()) {
             chunker.add(
-              QS_BOOKMARKS.decri_j_until_array_index_j_less_i,
+              QS_BOOKMARKS.SHARED_decri_j_until,
               (vis, j1, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_pivot, cur_depth) => {
 
                 if (j1 >= 0) {
@@ -423,14 +451,14 @@ export function run_QS(is_qs_median_of_3) {
         } while (i <= j && pivot_value() < a[j]);
 
         if (boolShouldAnimate()) {
-          chunker.add(QS_BOOKMARKS.if_j_greater_i);
+          chunker.add(QS_BOOKMARKS.SHARED_if_j_greater_i);
         }
         if (i < j) {
 
           swapAction(
             boolShouldAnimate()
-              ? QS_BOOKMARKS.swap_array_i_j_vals
-              : QS_BOOKMARKS.done_qs,
+              ? QS_BOOKMARKS.SHARED_swap_array_i_j_vals
+              : QS_BOOKMARKS.SHARED_done_qs,
             i,
             j
           );
@@ -440,10 +468,12 @@ export function run_QS(is_qs_median_of_3) {
       pivot_index = i
 
       // swap pivot with i
+
+      // TODO
       swapAction(
         boolShouldAnimate()
           ? QS_BOOKMARKS.swap_pivot_into_correct_position
-          : QS_BOOKMARKS.done_qs,
+          : QS_BOOKMARKS.SHARED_done_qs,
         i,
         is_qs_median_of_3 ? right-1 : right
       );
@@ -451,6 +481,8 @@ export function run_QS(is_qs_median_of_3) {
 
 
       if (boolShouldAnimate()) {
+
+        // TODO
         chunker.add(
           QS_BOOKMARKS.swap_pivot_into_correct_position,
           (vis, cur_real_stack, cur_finished_stack_frames, cur_i, cur_j, cur_depth, cur_pivot) => {
@@ -487,7 +519,7 @@ export function run_QS(is_qs_median_of_3) {
       }
 
       if (boolShouldAnimate()) {
-        chunker.add(QS_BOOKMARKS.if_left_less_right, refresh_stack, [
+        chunker.add(QS_BOOKMARKS.SHARED_if_left_less_right, refresh_stack, [
           real_stack,
           finished_stack_frames,
         ]);
@@ -497,7 +529,7 @@ export function run_QS(is_qs_median_of_3) {
         [pivot, a] = partition(a, left, right, depth);
 
         if (depth === 0 || isRecursionExpanded()) {
-          chunker.add(QS_BOOKMARKS.quicksort_left_to_i_minus_1, refresh_stack, [
+          chunker.add(QS_BOOKMARKS.SHARED_quicksort_left_to_i_minus_1, refresh_stack, [
             real_stack,
             finished_stack_frames,
           ]);
@@ -505,7 +537,7 @@ export function run_QS(is_qs_median_of_3) {
           // this part animates the recursion when it is collapsed
           // can also add a function to animate the swap actions in one step here instead of in the partition function
           chunker.add(
-            QS_BOOKMARKS.quicksort_left_to_i_minus_1,
+            QS_BOOKMARKS.SHARED_quicksort_left_to_i_minus_1,
             (vis, low, high) => {
               for (let i = low; i <= high; i++) {
                 // inclusive to make sure pivot is sorted at end
@@ -519,13 +551,13 @@ export function run_QS(is_qs_median_of_3) {
         QuickSort(a, left, pivot - 1, `${left}/${pivot - 1}`, depth + 1);
 
         if (depth === 0 || isRecursionExpanded()) {
-          chunker.add(QS_BOOKMARKS.quicksort_i_plus_1_to_right, refresh_stack, [
+          chunker.add(QS_BOOKMARKS.SHARED_quicksort_i_plus_1_to_right, refresh_stack, [
             real_stack,
             finished_stack_frames,
           ]);
         } else {
           chunker.add(
-            QS_BOOKMARKS.quicksort_left_to_i_minus_1,
+            QS_BOOKMARKS.SHARED_quicksort_left_to_i_minus_1,
             (vis, low, high) => {
               for (let i = low; i < high; i++) {
                 vis.array.sorted(i);
@@ -540,8 +572,8 @@ export function run_QS(is_qs_median_of_3) {
       // has a conditional to specify which line it jumps to depending on the expanding and collapsing
       else if (left < a.length) {
         let size_one_bookmark = isRecursionExpanded()
-          ? QS_BOOKMARKS.quicksort_left_to_i_minus_1
-          : QS_BOOKMARKS.done_qs;
+          ? QS_BOOKMARKS.SHARED_quicksort_left_to_i_minus_1
+          : QS_BOOKMARKS.SHARED_done_qs;
 
         chunker.add(
           size_one_bookmark,
@@ -562,7 +594,7 @@ export function run_QS(is_qs_median_of_3) {
     // ----------------------------------------------------------------------------------------------------------------------------
 
     chunker.add(
-      QS_BOOKMARKS.quicksort_left_to_right,
+      QS_BOOKMARKS.SHARED_quicksort_left_to_right,
       (vis, array) => {
         vis.array.set(array, 'quicksort');
         vis.array.setStack([]); // used for a custom stack visualisation
@@ -582,7 +614,7 @@ export function run_QS(is_qs_median_of_3) {
 
     // Fade out final node
     chunker.add(
-      QS_BOOKMARKS.done_qs,
+      QS_BOOKMARKS.SHARED_done_qs,
       (vis, idx) => {
         vis.array.fadeOut(idx);
         // fade all elements back in for final sorted state
