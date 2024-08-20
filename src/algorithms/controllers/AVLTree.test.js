b6cdf09672517d8f845ec3842e4aec7aeac75cc1
Ziyu-official <ziyu6@student.unimelb.edu.au>
2024-08-21 00:37:23 +0800

    UPDATE: add more simple test case
    but it cost 9.385s to do 5 very simple task, can someone run on your local browser to check this?

--- src/algorithms/controllers/AVLTree.test.js
+++ src/algorithms/controllers/AVLTree.test.js
@@ -13,12 +13,49 @@ const chunker = {
 };
 
 describe('AVLTreeInsertion', () => {
+  // Simple rotation: only envolves one rotate
   it('[Simple] No rotation', () => {
     const E = [10, 5, 15];
     const result = {
       10: { "height": 2, "left": 5, "right": 15, "par": null },
       5: { "height": 1, "left": null, "par": 10, "right": null },
       15: { "height": 1, "left": null, "par": 10, "right": null }
+    };
+    expect(AVLTreeInsertion.run(chunker, { nodes: E })).toEqual(result);
+  });
+  it('[Simple] Single left rotate', () => {
+    const E = [1, 2, 3];
+    const result = {
+      1: { "height": 1, "left": null, "right": null, "par": 2 },
+      2: { "height": 2, "left": 1, "right": 3, "par": null },
+      3: { "height": 1, "left": null, "right": null, "par": 2 }
+    };
+    expect(AVLTreeInsertion.run(chunker, { nodes: E })).toEqual(result);
+  });
+  it('[Simple] Single right rotate', () => {
+    const E = [3, 2, 1];
+    const result = {
+      1: { "height": 1, "left": null, "right": null, "par": 2 },
+      2: { "height": 2, "left": 1, "right": 3, "par": null },
+      3: { "height": 1, "left": null, "right": null, "par": 2 }
+    };
+    expect(AVLTreeInsertion.run(chunker, { nodes: E })).toEqual(result);
+  });
+  it('[Simple] Single LR rotate', () => {
+    const E = [3, 1, 2];
+    const result = {
+      1: { "height": 1, "left": null, "right": null, "par": 2 },
+      2: { "height": 2, "left": 1, "right": 3, "par": null },
+      3: { "height": 1, "left": null, "right": null, "par": 2 }
+    };
+    expect(AVLTreeInsertion.run(chunker, { nodes: E })).toEqual(result);
+  });
+  it('[Simple] Single RL rotate', () => {
+    const E = [1, 3, 2];
+    const result = {
+      1: { "height": 1, "left": null, "right": null, "par": 2 },
+      2: { "height": 2, "left": 1, "right": 3, "par": null },
+      3: { "height": 1, "left": null, "right": null, "par": 2 }
     };
     expect(AVLTreeInsertion.run(chunker, { nodes: E })).toEqual(result);
   });
