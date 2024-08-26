1270de53e8ac1cc152830396554b5da7c8484893
lee-naish <dr.lee.naish@gmail.com>
2024-08-26 15:42:15 +1000

    Can move graph nodes with mouse

--- src/algorithms/controllers/prim.js
+++ src/algorithms/controllers/prim.js
@@ -18,7 +18,7 @@ export default {
     };
   },
 
-  run(chunker, { edgeValueMatrix, coordsMatrix, startNode, endNodes}) {
+  run(chunker, { edgeValueMatrix, coordsMatrix, startNode, endNodes, moveNode}) {
     // String Variables used in displaying algo
     const algNameStr = 'prim';
     const dashStr = '-';
@@ -132,6 +132,7 @@ export default {
       (vis, edgeArray, coordsArray) => {
         vis.graph.directed(false);
         vis.graph.weighted(true);
+        vis.graph.moveNodeFn(moveNode);
         vis.graph.set(edgeArray, Array.from({ length: numVertices }, (v, k) => (k + 1)),coordsArray);
       },
       [E, coords]
