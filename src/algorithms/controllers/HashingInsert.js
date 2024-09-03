754bdcfc6604a6072bdd7e91c8e99a27c70027e5
joshuastefanolapitan <josh.lapitan@icloud.com>
2024-09-03 16:31:32 +1000

    Updating functions

--- src/algorithms/controllers/HashingInsert.js
+++ src/algorithms/controllers/HashingInsert.js
@@ -1,19 +1,45 @@
 export default {
     initVisualisers() {
+        return null;
     },
 
-    hashInit(tableSize) {
-        let table = new Array(tableSize);
+    run(chunker, params) {
 
-        return table;
+        function hashInit(tableSize) {
+            let table = new Array(tableSize);
+
+            return table;
+        }
+
+        function hash1(k) {
+            return k % 11;
+        }
+
+        function hashInsert(table, key) { // add mode parameter with case for
+
+            // get initial hash index
+            let i = hash1(key);
+
+            // linear probing collision handling
+            while (typeof table[i] !== 'undefined' && table[i] !== null) {
+                i = i + 1;
+                if (i > 10) {
+                    i = 0;
+                }
+            }
+            table[i] = key;
+        }
+
+        function hashDelete(table, key) {
+            let i = hash1(key);
+
+            while (table[i] != key) {
+                i = i + 1;
+                if (i > 10) {
+                    i = 0;
+                }
+            }
+            table[i] = null;
+        }
     },
-
-    hashInsert(table, key) {
-    },
-
-    hashDelete(table, key) {
-    }
-
-    // run(chunker, { nodes }) {
-    // },
-}
+};
