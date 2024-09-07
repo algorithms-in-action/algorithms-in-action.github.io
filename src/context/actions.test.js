37182cffdf5e76984d5fec3ccb25d6f8fc08811c
Min <minthuh@student.unimelb.edu.au>
2024-09-07 23:54:37 +1000

    Revert "Changed formatting for 'Sort' algos"
    This reverts commit 34ec1ab9860a65f596287d535820d2a2a9150e23.

--- src/context/actions.test.js
+++ src/context/actions.test.js
@@ -23,9 +23,9 @@ describe('my beverage', () => {
 
 // describe('NEXT_LINE action', () => {
 //   it('moves to next line', () => {
-//     const  = GlobalActions.LOAD_ALGORITHM(undefined, { name: 'binaryTreeSearch' });
-//     const newState = GlobalActions.NEXT_LINE();
-//     expect(newState.bookmark).not.toBe(.bookmark);
+//     const initialState = GlobalActions.LOAD_ALGORITHM(undefined, { name: 'binaryTreeSearch' });
+//     const newState = GlobalActions.NEXT_LINE(initialState);
+//     expect(newState.bookmark).not.toBe(initialState.bookmark);
 //   });
 // });
 
@@ -43,16 +43,16 @@ describe('my beverage', () => {
 
 // describe('NEXT_LINE action', () => {
 //   it('moves to next line', () => {
-//     const  = GlobalActions.LOAD_ALGORITHM(undefined, { name: 'binaryTreeSearch' });
-//     const newState = GlobalActions.NEXT_LINE();
-//     expect(newState.bookmark).not.toBe(.bookmark);
+//     const initialState = GlobalActions.LOAD_ALGORITHM(undefined, { name: 'binaryTreeSearch' });
+//     const newState = GlobalActions.NEXT_LINE(initialState);
+//     expect(newState.bookmark).not.toBe(initialState.bookmark);
 //   });
 // });
 
 // describe('PREV_LINE action', () => {
 //   it('moves to the previous line', () => {
-//     const  = GlobalActions.LOAD_ALGORITHM(undefined, { name: 'binaryTreeSearch' });
-//     const newState = GlobalActions.PREV_LINE(GlobalActions.NEXT_LINE());
-//     expect(newState.bookmark).toBe(.bookmark);
+//     const initialState = GlobalActions.LOAD_ALGORITHM(undefined, { name: 'binaryTreeSearch' });
+//     const newState = GlobalActions.PREV_LINE(GlobalActions.NEXT_LINE(initialState));
+//     expect(newState.bookmark).toBe(initialState.bookmark);
 //   });
 // });
