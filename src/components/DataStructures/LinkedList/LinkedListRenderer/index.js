100259c8ff10dda905bc7d92114e623fb3736477
yixingcherryxu1 <yixingx@student.unimelb.edu.au>
2024-09-25 15:42:08 +1000

    updated linked list to support multiple and add in ids, updated linked list tracer to support changes

--- src/components/DataStructures/LinkedList/LinkedListRenderer/index.js
+++ src/components/DataStructures/LinkedList/LinkedListRenderer/index.js
@@ -4,44 +4,51 @@ import Renderer from '../../common/Renderer/index';
 import styles from './LinkedListRenderer.module.scss';
 import { classes } from '../../common/util';
 
-class LinkedListRenderer extends Renderer{
+class LinkedListRenderer extends Renderer {
     constructor(props) {
         super(props);
 
         this.state = {
-            nodes: props.data.nodes, // assuming the linked list data is passed as props
+            lists: props.data.lists, // assuming the linked lists are passed as props
         };
     }
 
     renderData() {
-        const { nodes } = this.state;
+        const { lists } = this.state;
 
         return (
-            <div className={styles.linkedListContainer}>
+            <div className={styles.multiLinkedListContainer}>
                 <AnimateSharedLayout>
-                    {nodes.map((node, index) => (
-                        <div className={styles.nodeContainer} key={index}>
-                            <motion.div
-                                layoutId={`node-${index}`}
-                                className={styles.node}
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                transition={{ type: 'spring', stiffness: 100 }}
-                            >
-                                <div className={styles.value}>{node.value}</div>
-                                <div className={styles.pointer}>
-                                    {node.next !== null ? (
-                                        <motion.div
-                                            className={styles.arrow}
-                                            layoutId={`arrow-${index}`}
-                                        >
-                                            →
-                                        </motion.div>
-                                    ) : (
-                                        <motion.div className={styles.null}>null</motion.div>
-                                    )}
+                    {lists.map((list, listIndex) => (
+                        <div className={styles.linkedListContainer} key={`list-${listIndex}`}>
+                            <div className={styles.listIdentifier}>
+                                Linked List {listIndex + 1}
+                            </div>
+                            {list.nodes.map((node, nodeIndex) => (
+                                <div className={styles.nodeContainer} key={`node-${listIndex}-${nodeIndex}`}>
+                                    <motion.div
+                                        layoutId={`list-${listIndex}-node-${nodeIndex}`}
+                                        className={styles.node}
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        transition={{ type: 'spring', stiffness: 100 }}
+                                    >
+                                        <div className={styles.value}>{node.value}</div>
+                                        <div className={styles.pointer}>
+                                            {node.next !== null ? (
+                                                <motion.div
+                                                    className={styles.arrow}
+                                                    layoutId={`list-${listIndex}-arrow-${nodeIndex}`}
+                                                >
+                                                    →
+                                                </motion.div>
+                                            ) : (
+                                                <motion.div className={styles.null}>null</motion.div>
+                                            )}
+                                        </div>
+                                    </motion.div>
                                 </div>
-                            </motion.div>
+                            ))}
                         </div>
                     ))}
                 </AnimateSharedLayout>
@@ -51,3 +58,63 @@ class LinkedListRenderer extends Renderer{
 }
 
 export default LinkedListRenderer;
+
+
+
+
+
+
+
+// import React from 'react';
+// import { motion, AnimateSharedLayout } from 'framer-motion';
+// import Renderer from '../../common/Renderer/index';
+// import styles from './LinkedListRenderer.module.scss';
+// import { classes } from '../../common/util';
+
+// class LinkedListRenderer extends Renderer{
+//     constructor(props) {
+//         super(props);
+
+//         this.state = {
+//             nodes: props.data.nodes, // assuming the linked list data is passed as props
+//         };
+//     }
+
+//     renderData() {
+//         const { nodes } = this.state;
+
+//         return (
+//             <div className={styles.linkedListContainer}>
+//                 <AnimateSharedLayout>
+//                     {nodes.map((node, index) => (
+//                         <div className={styles.nodeContainer} key={index}>
+//                             <motion.div
+//                                 layoutId={`node-${index}`}
+//                                 className={styles.node}
+//                                 initial={{ opacity: 0 }}
+//                                 animate={{ opacity: 1 }}
+//                                 transition={{ type: 'spring', stiffness: 100 }}
+//                             >
+//                                 <div className={styles.value}>{node.value}</div>
+//                                 <div className={styles.pointer}>
+//                                     {node.next !== null ? (
+//                                         <motion.div
+//                                             className={styles.arrow}
+//                                             layoutId={`arrow-${index}`}
+//                                         >
+//                                             →
+//                                         </motion.div>
+//                                     ) : (
+//                                         <motion.div className={styles.null}>null</motion.div>
+//                                     )}
+//                                 </div>
+//                             </motion.div>
+//                         </div>
+//                     ))}
+//                 </AnimateSharedLayout>
+//             </div>
+//         );
+//     }
+// }
+
+// export default LinkedListRenderer;
--- src/components/DataStructures/LinkedList/LinkedListTracer.js
+++ src/components/DataStructures/LinkedList/LinkedListTracer.js
@@ -7,10 +7,7 @@ import LinkedListRenderer from './LinkedListRenderer'; // Assume we have a rende
 
 class LinkedListTracer {
     constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-        this.data = [];
+        this.lists = []; // Array of linked lists
         this.chartTracer = null;
     }
 
@@ -20,51 +17,66 @@ class LinkedListTracer {
 
     init() {
         this.chartTracer = null;
-        this.data = [];
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
+        this.lists = [];
     }
 
-    // Sets the linked list data structure
-    set(linkedList = [], algo) {
+    // Sets multiple linked lists
+    set(listsData = []) {
         this.init();
-        linkedList.forEach((value) => this.append(value));
+        listsData.forEach((listData) => {
+            const list = { head: null, tail: null, size: 0, data: [] };
+            listData.forEach((value) => this.appendToList(value, list));
+            this.lists.push(list);
+        });
         this.syncChartTracer();
     }
 
-    // Appends a value to the linked list
-    append(value) {
+    // Appends a value to a specific linked list
+    appendToList(value, list) {
         const newNode = { value, next: null, variables: [] };
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
+        if (!list.head) {
+            list.head = newNode;
+            list.tail = newNode;
         } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
+            list.tail.next = newNode;
+            list.tail = newNode;
         }
-        this.data.push(newNode);
-        this.size++;
+        list.data.push(newNode);
+        list.size++;
     }
 
-    // Prepends a value to the linked list
-    prepend(value) {
-        const newNode = { value, next: this.head, variables: [] };
-        this.head = newNode;
-        if (!this.tail) {
-            this.tail = newNode;
+    // Appends a value to a specific list by index
+    append(value, listIndex = 0) {
+        if (listIndex >= 0 && listIndex < this.lists.length) {
+            this.appendToList(value, this.lists[listIndex]);
+            this.syncChartTracer();
         }
-        this.data.unshift(newNode);
-        this.size++;
     }
 
-    // Removes a node at a specific index
-    removeAt(index) {
-        if (index < 0 || index >= this.size) return;
-        let current = this.head;
+    // Prepends a value to a specific list
+    prepend(value, listIndex = 0) {
+        if (listIndex >= 0 && listIndex < this.lists.length) {
+            const list = this.lists[listIndex];
+            const newNode = { value, next: list.head, variables: [] };
+            list.head = newNode;
+            if (!list.tail) {
+                list.tail = newNode;
+            }
+            list.data.unshift(newNode);
+            list.size++;
+            this.syncChartTracer();
+        }
+    }
+
+    // Removes a node at a specific index from a specific list
+    removeAt(index, listIndex = 0) {
+        const list = this.lists[listIndex];
+        if (!list || index < 0 || index >= list.size) return;
+
+        let current = list.head;
         if (index === 0) {
-            this.head = current.next;
-            this.data.shift();
+            list.head = current.next;
+            list.data.shift();
         } else {
             let previous = null;
             for (let i = 0; i < index; i++) {
@@ -72,93 +84,280 @@ class LinkedListTracer {
                 current = current.next;
             }
             previous.next = current.next;
-            if (index === this.size - 1) {
-                this.tail = previous;
+            if (index === list.size - 1) {
+                list.tail = previous;
             }
-            this.data.splice(index, 1);
+            list.data.splice(index, 1);
         }
-        this.size--;
+        list.size--;
+        this.syncChartTracer();
     }
 
-    // Patches/highlights a node
-    patch(index, value) {
-        if (index >= 0 && index < this.size) {
-            this.data[index].value = value;
-        }
-    }
-
-    // Removes patch/highlight
-    depatch(index) {
-        if (index >= 0 && index < this.size) {
-            this.data[index].value = this.data[index].originalValue;
+    // Swaps two nodes by index within a specific list
+    swapNodes(index1, index2, listIndex = 0) {
+        const list = this.lists[listIndex];
+        if (list && index1 >= 0 && index1 < list.size && index2 >= 0 && index2 < list.size) {
+            const temp = list.data[index1].value;
+            list.data[index1].value = list.data[index2].value;
+            list.data[index2].value = temp;
+            this.syncChartTracer();
         }
     }
 
-    // Selects a node or a range of nodes
-    select(startIndex, endIndex = startIndex) {
+    // Patches/highlights a node at a specific index in a specific list
+    patch(index, value, listIndex = 0) {
+        const list = this.lists[listIndex];
+        if (list && index >= 0 && index < list.size) {
+            list.data[index].value = value;
+        }
+    }
+
+    // Removes patch/highlight from a node
+    depatch(index, listIndex = 0) {
+        const list = this.lists[listIndex];
+        if (list && index >= 0 && index < list.size) {
+            list.data[index].value = list.data[index].originalValue;
+        }
+    }
+
+    // Selects a node or a range of nodes in a specific list
+    select(startIndex, endIndex = startIndex, listIndex = 0) {
+        const list = this.lists[listIndex];
         for (let i = startIndex; i <= endIndex; i++) {
-            if (i >= 0 && i < this.size) {
-                this.data[i].selected = true;
+            if (i >= 0 && i < list.size) {
+                list.data[i].selected = true;
             }
         }
     }
 
-    // Deselects a node or a range of nodes
-    deselect(startIndex, endIndex = startIndex) {
+    // Deselects a node or a range of nodes in a specific list
+    deselect(startIndex, endIndex = startIndex, listIndex = 0) {
+        const list = this.lists[listIndex];
         for (let i = startIndex; i <= endIndex; i++) {
-            if (i >= 0 && i < this.size) {
-                this.data[i].selected = false;
+            if (i >= 0 && i < list.size) {
+                list.data[i].selected = false;
             }
         }
     }
 
-    // Swaps two nodes by index
-    swapNodes(index1, index2) {
-        if (index1 >= 0 && index1 < this.size && index2 >= 0 && index2 < this.size) {
-            const temp = this.data[index1].value;
-            this.data[index1].value = this.data[index2].value;
-            this.data[index2].value = temp;
+    // Adds a variable to a specific node in a specific list
+    addVariable(variable, nodeIndex, listIndex = 0) {
+        const list = this.lists[listIndex];
+        if (list && nodeIndex >= 0 && nodeIndex < list.size) {
+            list.data[nodeIndex].variables.push(variable);
+            this.syncChartTracer();
         }
     }
 
-    // Adds a variable to a specific node
-    addVariable(variable, index) {
-        if (index >= 0 && index < this.size) {
-            this.data[index].variables.push(variable);
-        }
-    }
-
-    // Removes a variable from all nodes
+    // Removes a variable from all nodes in all lists
     removeVariable(variable) {
-        this.data.forEach((node) => {
-            node.variables = node.variables.filter((val) => val !== variable);
+        this.lists.forEach(list => {
+            list.data.forEach((node) => {
+                node.variables = node.variables.filter((val) => val !== variable);
+            });
         });
+        this.syncChartTracer();
     }
 
-    // Clears all variables from the linked list
+    // Clears all variables from all nodes in all lists
     clearVariables() {
-        this.data.forEach((node) => {
-            node.variables = [];
+        this.lists.forEach(list => {
+            list.data.forEach((node) => {
+                node.variables = [];
+            });
         });
+        this.syncChartTracer();
     }
 
-    // Assigns a variable to a specific node, removing it from all others
-    assignVariable(variable, index) {
+    // Assigns a variable to a specific node in a specific list, removing it from all others
+    assignVariable(variable, nodeIndex, listIndex = 0) {
         this.clearVariables();
-        if (index >= 0 && index < this.size) {
-            this.addVariable(variable, index);
-        }
+        this.addVariable(variable, nodeIndex, listIndex);
     }
 
     // Synchronizes the chart tracer
     syncChartTracer() {
-        if (this.chartTracer) this.chartTracer.data = this.data;
+        if (this.chartTracer) {
+            this.chartTracer.data = this.lists.map(list => list.data); // Sync all lists with the tracer
+        }
     }
 
-    // Returns a string representation of the linked list
+    // Returns a string representation of all linked lists
     stringTheContent() {
-        return this.data.map((node) => node.value).join(' -> ');
+        return this.lists
+            .map((list, index) => `List ${index + 1}: ${list.data.map((node) => node.value).join(' -> ')}`)
+            .join('\n');
     }
 }
 
 export default LinkedListTracer;
+
+
+
+
+
+
+// /* eslint-disable class-methods-use-this */
+// /* eslint-disable consistent-return */
+// /* eslint-disable no-plusplus */
+// /* eslint-disable max-len */
+// import { cloneDeepWith } from 'lodash';
+// import LinkedListRenderer from './LinkedListRenderer'; // Assume we have a renderer for linked lists
+
+// class LinkedListTracer {
+//     constructor() {
+//         this.head = null;
+//         this.tail = null;
+//         this.size = 0;
+//         this.data = [];
+//         this.chartTracer = null;
+//     }
+
+//     getRendererClass() {
+//         return LinkedListRenderer;
+//     }
+
+//     init() {
+//         this.chartTracer = null;
+//         this.data = [];
+//         this.head = null;
+//         this.tail = null;
+//         this.size = 0;
+//     }
+
+//     // Sets the linked list data structure
+//     set(linkedList = [], algo) {
+//         this.init();
+//         linkedList.forEach((value) => this.append(value));
+//         this.syncChartTracer();
+//     }
+
+//     // Appends a value to the linked list
+//     append(value) {
+//         const newNode = { value, next: null, variables: [] };
+//         if (!this.head) {
+//             this.head = newNode;
+//             this.tail = newNode;
+//         } else {
+//             this.tail.next = newNode;
+//             this.tail = newNode;
+//         }
+//         this.data.push(newNode);
+//         this.size++;
+//     }
+
+//     // Prepends a value to the linked list
+//     prepend(value) {
+//         const newNode = { value, next: this.head, variables: [] };
+//         this.head = newNode;
+//         if (!this.tail) {
+//             this.tail = newNode;
+//         }
+//         this.data.unshift(newNode);
+//         this.size++;
+//     }
+
+//     // Removes a node at a specific index
+//     removeAt(index) {
+//         if (index < 0 || index >= this.size) return;
+//         let current = this.head;
+//         if (index === 0) {
+//             this.head = current.next;
+//             this.data.shift();
+//         } else {
+//             let previous = null;
+//             for (let i = 0; i < index; i++) {
+//                 previous = current;
+//                 current = current.next;
+//             }
+//             previous.next = current.next;
+//             if (index === this.size - 1) {
+//                 this.tail = previous;
+//             }
+//             this.data.splice(index, 1);
+//         }
+//         this.size--;
+//     }
+
+//     // Patches/highlights a node
+//     patch(index, value) {
+//         if (index >= 0 && index < this.size) {
+//             this.data[index].value = value;
+//         }
+//     }
+
+//     // Removes patch/highlight
+//     depatch(index) {
+//         if (index >= 0 && index < this.size) {
+//             this.data[index].value = this.data[index].originalValue;
+//         }
+//     }
+
+//     // Selects a node or a range of nodes
+//     select(startIndex, endIndex = startIndex) {
+//         for (let i = startIndex; i <= endIndex; i++) {
+//             if (i >= 0 && i < this.size) {
+//                 this.data[i].selected = true;
+//             }
+//         }
+//     }
+
+//     // Deselects a node or a range of nodes
+//     deselect(startIndex, endIndex = startIndex) {
+//         for (let i = startIndex; i <= endIndex; i++) {
+//             if (i >= 0 && i < this.size) {
+//                 this.data[i].selected = false;
+//             }
+//         }
+//     }
+
+//     // Swaps two nodes by index
+//     swapNodes(index1, index2) {
+//         if (index1 >= 0 && index1 < this.size && index2 >= 0 && index2 < this.size) {
+//             const temp = this.data[index1].value;
+//             this.data[index1].value = this.data[index2].value;
+//             this.data[index2].value = temp;
+//         }
+//     }
+
+//     // Adds a variable to a specific node
+//     addVariable(variable, index) {
+//         if (index >= 0 && index < this.size) {
+//             this.data[index].variables.push(variable);
+//         }
+//     }
+
+//     // Removes a variable from all nodes
+//     removeVariable(variable) {
+//         this.data.forEach((node) => {
+//             node.variables = node.variables.filter((val) => val !== variable);
+//         });
+//     }
+
+//     // Clears all variables from the linked list
+//     clearVariables() {
+//         this.data.forEach((node) => {
+//             node.variables = [];
+//         });
+//     }
+
+//     // Assigns a variable to a specific node, removing it from all others
+//     assignVariable(variable, index) {
+//         this.clearVariables();
+//         if (index >= 0 && index < this.size) {
+//             this.addVariable(variable, index);
+//         }
+//     }
+
+//     // Synchronizes the chart tracer
+//     syncChartTracer() {
+//         if (this.chartTracer) this.chartTracer.data = this.data;
+//     }
+
+//     // Returns a string representation of the linked list
+//     stringTheContent() {
+//         return this.data.map((node) => node.value).join(' -> ');
+//     }
+// }
+
+// export default LinkedListTracer;
