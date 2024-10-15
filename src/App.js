2adc28c718ccf80695159b40bfe4eaf46789a7b8
Charlie Wang <84770255+Guhh0001@users.noreply.github.com>
2024-10-15 12:43:36 +1100

    all fix

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import AlgorithmMenu from './components/AlgorithmMenu';
 import { ReactComponent as Circle } from './assets/icons/circle.svg';
 import { ReactComponent as Direction } from './assets/icons/direction.svg';
 import { GlobalProvider } from './context/GlobalState';
+import LeftPanel from './components/left-panel';
 import RightPanel from './components/right-panel';
 import MidPanel from './components/mid-panel';
 import ControlPanel from './components/mid-panel/ControlPanel';
 import Settings from './components/top/Settings';
 import {
-  resizeWindow, startRightDrag, startBottomDrag, endDrag, onDrag, collapseBottomDrag, collapseRightDrag, addEvent,
+  resizeWindow, startLeftDrag, startRightDrag, startBottomDrag, endDrag, onDrag, collapseLeftDrag, collapseBottomDrag, collapseRightDrag, addEvent,
 } from './BorderResize';
 import {
   setTheme,
@@ -24,32 +25,28 @@ import {
 } from './components/top/helper';
 
 const DEFAULT_FONT_INCREMENT = 0;
+const LEFT_FONT_SIZE = 15;
 const MID_FONT_SIZE = 15;
 const RIGHT_FONT_SIZE = 15;
 
 function App() {
   useEffect(() => {
-    window.addEventListener('resize', (event) => {
-      resizeWindow(event);
-    });
-    // eslint-disable-next-line no-unused-vars
-    return (_) => { window.removeEventListener('resize', resizeWindow); };
-  });
+    window.addEventListener('resize', resizeWindow);
+    return () => {
+      window.removeEventListener('resize', resizeWindow);
+    };
+  }, []);
 
-  // add mouseout event listener to 'document' when App first mount
   useEffect(() => {
     const mouseOutCallback = (e) => {
-      // e = e || window.event;
       const from = e.relatedTarget || e.toElement;
       if (!from || from.nodeName === 'HTML') {
-        // End dragging when mouse out of html
         endDrag();
       }
     };
 
-    addEvent(document, 'mouseout', mouseOutCallback);
+    document.addEventListener('mouseout', mouseOutCallback);
 
-    // do not forget to remove event listener when the App component unmount
     return () => {
       document.removeEventListener('mouseout', mouseOutCallback);
     };
@@ -66,7 +63,6 @@ function App() {
     setFontSizeIncrease(fontSizeIncrease + val);
   };
 
-
   const initAlgoColor = () => {
     const algoTheme = getWithExpiry(ALGO_THEME_KEY);
     if (algoTheme === null) {
@@ -82,14 +78,12 @@ function App() {
     setAlgoTheme(id);
   };
 
-
   const initSystemColor = () => {
     const theme = getWithExpiry(SYSTEM_THEME_KEY);
     if (theme === null) {
       setTheme(getSystemColorMode());
       return getSystemColorMode();
     }
-
     return theme;
   };
 
@@ -105,9 +99,8 @@ function App() {
   });
 
   return (
-
     <GlobalProvider>
-      { isSettingVisible ? (
+      {isSettingVisible ? (
         <Settings
           onFontIncrease={onFontIncrease}
           onSetting={onSetting}
@@ -125,27 +118,41 @@ function App() {
         tabIndex="-1"
         onMouseMove={(event) => onDrag(event)}
       >
-        <div id="header">
-          <AlgorithmMenu />
-          <Header onSetting={onSetting} />
+        <div id="header" className="header-container">
+          <div className="header-left">
+            <AlgorithmMenu />
+          </div>
+          <div className="header-right">
+            <Header onSetting={onSetting} />
+          </div>
         </div>
-        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+        <div id="leftcol">
+          <LeftPanel
+            fontSize={LEFT_FONT_SIZE}
+            fontSizeIncrement={fontSizeIncrease}
+          />
+        </div>
         <div
-          id="tabpages"
+          id="leftdragbar"
           tabIndex="-1"
-          aria-label="Move tab pages"
-          onDoubleClick={collapseBottomDrag}
-          onMouseDown={startBottomDrag}
+          aria-label="Move left drag bar"
+          onDoubleClick={collapseLeftDrag}
+          onMouseDown={startLeftDrag}
           role="button"
           className="dragbar"
         >
-          <div id="draghandle" className="handle bottomHandle">
-            <Circle />
-            <Circle />
-            <Circle />
+          <div id="draghandle" className="handle">
+            <Direction id="leftdraghandle" />
           </div>
         </div>
-        <div id="rightdragbar"
+        <div id="tabpages">
+          <MidPanel
+            fontSize={MID_FONT_SIZE}
+            fontSizeIncrement={fontSizeIncrease}
+          />
+        </div>
+        <div
+          id="rightdragbar"
           tabIndex="-1"
           aria-label="Move right drag bar"
           onDoubleClick={collapseRightDrag}
@@ -165,6 +172,21 @@ function App() {
             fontSizeIncrement={fontSizeIncrease}
           />
         </div>
+        <div
+          id="bottomdragbar"
+          tabIndex="-1"
+          aria-label="Move bottom drag bar"
+          onDoubleClick={collapseBottomDrag}
+          onMouseDown={startBottomDrag}
+          role="button"
+          className="dragbar"
+        >
+          <div id="draghandle" className="handle bottomHandle">
+            <Circle />
+            <Circle />
+            <Circle />
+          </div>
+        </div>
         <div id="footer">
           <ControlPanel />
         </div>
@@ -172,6 +194,5 @@ function App() {
     </GlobalProvider>
   );
 }
-
 
 export default App;
