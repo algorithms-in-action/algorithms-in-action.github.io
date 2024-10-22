0f7b435e9c646e2d60c69b7818be6388649d559c
kraklo <evanliapakis@gmail.com>
2024-10-22 14:40:18 +1100

    feat: add URL support

--- src/algorithms/parameters/MSDRadixSortParam.js
+++ src/algorithms/parameters/MSDRadixSortParam.js
@@ -1,17 +1,26 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { URLContext } from '../../context/urlState';
 import { genRandNumList } from './helpers/ParamHelper';
 import ListParam from './helpers/ListParam';
+import PropTypes from 'prop-types';
+import { withAlgorithmParams } from './helpers/urlHelpers';
 import '../../styles/Param.scss';
 
 const DEFAULT_NODES = genRandNumList(10, 1, 100);
+const DEFAULT_MODE = 'sort';
 const MSD_RADIX_SORT = 'MSD Radix Sort';
 const MSDRS_SORT_EXAMPLE = 'Please follow the example provided: 0,1,2,3,4';
 
-function MSDRadixSortParam() {
+function MSDRadixSortParam({ mode, list }) {
     const [message, setMessage] = useState(null);
-    const [nodes, setNodes] = useState(DEFAULT_NODES);
+    const [localNodes, setLocalNodes] = useState(list || DEFAULT_NODES);
+    const { setNodes } = useContext(URLContext);
+
+    useEffect(() => {
+        setNodes(localNodes);
+    }, [localNodes]);
 
     return (
         <>
@@ -19,10 +28,10 @@ function MSDRadixSortParam() {
                 <ListParam
                     name="radixSortMSD"
                     buttonName="Sort"
-                    mode="sort"
+                    mode={mode || DEFAULT_MODE}
                     formClassName="formLeft"
-                    DEFAULT_VAL={nodes}
-                    SET_VAL={setNodes}
+                    DEFAULT_VAL={localNodes}
+                    SET_VAL={setLocalNodes}
                     ALGORITHM_NAME={MSD_RADIX_SORT}
                     EXAMPLE={MSDRS_SORT_EXAMPLE}
                     setMessage={setMessage}
@@ -35,4 +44,10 @@ function MSDRadixSortParam() {
     );
 }
 
-export default MSDRadixSortParam;
+MSDRadixSortParam.propTypes = {
+    alg: PropTypes.string.isRequired,
+    mode: PropTypes.string.isRequired,
+    list: PropTypes.string.isRequired,
+}
+
+export default withAlgorithmParams(MSDRadixSortParam);
