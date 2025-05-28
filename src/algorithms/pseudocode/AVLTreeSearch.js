8bb7aad1da1cf89c6af978d37f3c489138ca96f2
lee-naish <dr.lee.naish@gmail.com>
2025-05-28 11:04:42 +1000

    AVL terminology, search, BST

--- src/algorithms/pseudocode/AVLTreeSearch.js
+++ src/algorithms/pseudocode/AVLTreeSearch.js
@@ -9,31 +9,31 @@ AVLT_Search(t, k)  // return subtree whose root has key k; or NotFound  \\B AVL_
 \\In{
     while t not Empty   \\B while t not Empty
     \\In{
-        n = root(t)  \\B n = root(t)
-        \\Note{ Avoids confounding pointers and nodes  XXX not worth it???
-            could use key(t), left(t), right(t) below; make as similar to
-            AVLTree code as possible
+        \\Note{ Old code:
+            n = root(t)  \\B n = root(t)
+            Avoids confounding pointers and nodes  XXX not worth it???
+            now use t.key etc below; bookmarks not changed XXX
         \\Note}
-        if n.key = k    \\B if n.key = k
+        if t.key = k    \\B if n.key = k
         \\In{
             return t    \\B return t
             \\Expl{  We have found a node with the desired key k.
             \\Expl}
         \\In}
-        if n.key > k    \\B if n.key > k
+        if k < t.key    \\B if n.key > k
         \\Expl{  The BST condition is that nodes with keys less than the 
                 current node's key are to be found in the left subtree, and
                 nodes whose keys are greater are to be in the right subtree.
         \\Expl}
         \\In{
-            t <- n.left   \\B t <- n.left
+            t <- t.left   \\B t <- n.left
         \\In}
         else
         \\In{
-            t <- n.right    \\B t <- n.right
+            t <- t.right    \\B t <- n.right
         \\In}
-    return NotFound   \\B return NotFound
     \\In}
+    return NotFound   \\B return NotFound
 \\In}
 \\Code}
 `);
