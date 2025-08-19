830d33b8d2544ccbffeef72e6e54552827e84f34
Elliott Young <etyoung@student.unimelb.edu.au>
2025-08-19 13:07:48 +1000

    Modified ParamMsg.js, now, on errors, the ControlPanel will scroll the error message into view.

--- src/algorithms/parameters/helpers/ParamMsg.js
+++ src/algorithms/parameters/helpers/ParamMsg.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
-
 
 function ParamMsg({ logWarning, logTag, logMsg }) {
   const warningCol = '#FB3640';
   const successCol = '#52AA5E';
+
+  // Scroll logContainer into view
+  const ref = useRef(null);
+  useEffect(() => {
+    if (!ref.current) return;
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollIntoView
+    ref.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      inline: 'nearest',
+    });
+  }, [logTag, logMsg]); // Only when logMsg changes or appears for the first time.
+  // logTag added as well for future iterations if we have logTag changing but not logMsg.
+
   return (
-    <div className="logContainer">
+    <div ref={ref} className="logContainer">
       <span
         className="logTag"
         data-testid="logTag"
