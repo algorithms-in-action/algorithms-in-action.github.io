67c0450ecf17da8d7dfbaae9ec4cc3cdd53e4264
lee-naish <dr.lee.naish@gmail.com>
2026-01-15 12:27:45 +1100

    selectionSort pseuducode, animation ok
    background etc still needs work

--- src/algorithms/controllers/selectionSort.js
+++ src/algorithms/controllers/selectionSort.js
@@ -2,8 +2,8 @@
 import ArrayTracer from '../../components/DataStructures/Array/Array1DTracer';
 import { colors } from '../../components/DataStructures/colors';
 
-const selColor = colors.apple;   
-const minColor = colors.sky;
+const selColor = colors.leaf;   
+const minColor = colors.apple;
 const sortedColor = colors.stone;
 
 export default {
@@ -24,52 +24,57 @@ export default {
         const A = [...nodes];
         const n = A.length;
 
-        chunker.add(1, (vis, arr) => {
+        chunker.add('Main', (vis, arr) => {
             vis.array.set(arr, 'selectionsort');
         }, [A]);
 
         for (let i = 0; i < n - 1; i++) {
             let minIdx = i;
 
-            chunker.add(2, (vis, cur) => {
-                vis.array.selectColor(cur, selColor);
+            chunker.add('For_i', (vis, cur, n) => {
+                for (let i = cur; i < n; i++)
+                    vis.array.deselect(i); // XXX color interface???
                 vis.array.assignVariable('i', cur);
-            }, [i]);
+            }, [i, n]);
 
+            chunker.add('Init_min', (vis, cur) => {
+                vis.array.assignVariable('min', cur);
+                vis.array.selectColor(cur, minColor);
+            }, [i]);
             for (let j = i + 1; j < n; j++) {
-                chunker.add(3, (vis, cur) => {
-                    vis.array.selectColor(cur, minColor);
+                chunker.add('For_j', (vis, cur) => {
+                    vis.array.selectColor(cur, selColor);
                     vis.array.assignVariable('j', cur);
                 }, [j]);
 
-                if (A[j] < A[minIdx]) {
-                    minIdx = j;
-                    chunker.add(4, (vis, newMin) => {
-                        vis.array.assignVariable('min', newMin);
-                    }, [minIdx]);
-                }
-
-                chunker.add(5, (vis, cur) => {
-                    vis.array.depatch(cur);
-                    vis.array.removeVariable('j');
+                chunker.add('IfA[j]<A[min]', (vis, cur) => {
+                    // vis.array.depatch(cur);
+                    // vis.array.removeVariable('j');
                 }, [j]);
+                if (A[j] < A[minIdx]) {
+                    chunker.add('min<-j', (vis, newMin, oldMin) => {
+                        vis.array.assignVariable('min', newMin);
+                        vis.array.selectColor(newMin, minColor);
+                        vis.array.selectColor(oldMin, selColor);
+                    }, [j, minIdx]);
+                    minIdx = j;
+                }
             }
 
-            if (minIdx !== i) {
+            // if (minIdx !== i) {
                 [A[i], A[minIdx]] = [A[minIdx], A[i]];
-                chunker.add(6, (vis, a, b) => {
+                chunker.add('Swap', (vis, a, b) => {
                     vis.array.swapElements(a, b);
+                    vis.array.removeVariable('j');
+                    vis.array.removeVariable('min');
+                    vis.array.sorted(a);
                 }, [i, minIdx]);
-            }
+            // }
 
-            chunker.add(7, (vis, idx) => {
-                vis.array.sorted(idx);
-                vis.array.removeVariable('i');
-                vis.array.removeVariable('min');
-            }, [i]);
         }
 
-        chunker.add(8, (vis, idx) => {
+        chunker.add('Done', (vis, idx) => {
+            vis.array.removeVariable('i');
             vis.array.sorted(idx);
         }, [n - 1]);
 
--- src/algorithms/pseudocode/selectionSort.js
+++ src/algorithms/pseudocode/selectionSort.js
@@ -1,12 +1,78 @@
 import parse from '../../pseudocode/parse';
 
 export default parse(`
+\\Note{
+Selection sort draft, with some help from ChatGPT. It's not too bad at
+producing pseudocode and comments.  More structuring is needed and care
+is needed (of course) - it gets some things wrong.
+
+ChatGPT version "write a high level pseudocode description of the
+selection sort algorithm":
+SelectionSort(array A)
+    n ← length of A
+
+    // Repeat for each position in the array (except the last one)
+    for i from 0 to n - 2 do
+        // Assume the smallest element is at position i
+        minIndex ← i
+
+        // Look for a smaller element in the rest of the array
+        for j from i + 1 to n - 1 do
+            if A[j] < A[minIndex] then
+                minIndex ← j
+
+        // Swap the smallest found with the element at position i
+        if minIndex ≠ i then
+            swap A[i] and A[minIndex]
+\\Note}
+
+\\Code{
+Main
+SelectionSort(A, n) // sort A[1]...A[n] in increasing order \\B Main
+\\In{
+    for i <- 1 to n - 1 \\B For_i
+    \\Expl{ At each iteration the elements A[1]...A[i-1] are
+        in their final position. They are the smallest i-1 elements, in
+        sorted order. We stop at A[n-1] (at this point A[n] must be the
+        largest element).
+    \\Expl}
+    \\In{
+        Swap A[i] with the minimum unsorted element \\Ref NextEl
+        \\Expl{ The elements up to A[i-1] are already sorted, in their
+          final positions. This step extends the sorted region to A[i].
+        \\Expl}
+    \\In}
+    Done \\B Done
+\\In} 
+\\Code} 
+
+\\Code{
+NextEl
+min <- i // initially assume A[i] is smallest \\B Init_min
+\\Expl{ min is set to the index of the smallest element of A[i]...A[n].
+  Initially we set it to i then scan the rest of A, updating min
+  whenever we find a smaller element.
+\\Expl}
+for j <- i + 1 to n // search A[i+1]...A[n] for smaller elements \\B For_j
+    \\In{
+    if A[j] < A[min] // if we have found a smaller element \\B IfA[j]<A[min]
+        \\In{
+        min <- j // update index of smallest \\B min<-j
+        \\In}
+    \\In}
+Swap(A[i], A[min]) // Swap smallest into its correct position, A[i] \\B Swap
+\\Code} 
+`);
+
+/*
+// student version, with bookmarks improved
+export default parse(`
 \\Note{  REAL specification of selection sort animation
         \\Note}
 
         \\Code{
         Main
-        SelectionSort(A, n) // Sort array A[1]..A[n] in ascending order. \\B 1
+        SelectionSort(A, n) // Sort array A[1]..A[n] in ascending order. \\B Main
         \\Expl{  Selection Sort 每一趟在未排序区中选择最小元素，
                  放到已排序区的末尾。
                  简单直观，但不稳定，时间复杂度 O(n^2)。
@@ -16,22 +82,22 @@ export default parse(`
             \\Expl{  外层循环：确定 A[i] 的最终位置。
             \\Expl}
         \\In}
-        // Done \\B 8
+        // Done \\B Done
         \\Code}
 
         \\Code{
         ForLoop_i
-        for i <- 1 to n-1 \\B 2
+        for i <- 1 to n-1 \\B For_i
         \\Expl{  i 从 1 开始，表示当前要放置的最小元素位置。
         \\Expl}
         \\In{
-            min <- i \\B 3
+            min <- i \\B Init_min
             \\Expl{  假设当前位置就是最小值下标。
             \\Expl}
             ForLoop_j(i+1, n)   \\Ref ForLoop_j
             \\Expl{  内层循环：在 A[i+1..n] 中找更小的元素。
             \\Expl}
-            SwapIfNeed(A[i], A[min]) \\B 8
+            SwapIfNeed(A[i], A[min]) \\B Swap
             \\Expl{  找到更小元素就交换，使 A[i] 成为当前最小。
             \\Expl}
         \\In}
@@ -39,17 +105,18 @@ export default parse(`
 
         \\Code{
         ForLoop_j
-        for j <- i+1 to n \\B 4
+        for j <- i+1 to n \\B For_j
         \\Expl{  遍历未排序区的每一个元素。
         \\Expl}
         \\In{
-            if A[j] < A[min] \\B 5
+            if A[j] < A[min] \\B IfA[j]<A[min]
             \\Expl{  如果比当前最小值还小，则更新最小值下标。
             \\Expl}
             \\In{
-                min <- j \\B 6
+                min <- j \\B min<-j
             \\In}
         \\In}
-        EndFor_j // end for j \\B 7
+        EndFor_j // end for j
         \\Code}
 `);
+*/
